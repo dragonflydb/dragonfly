0f0638cbfb845e9fefbabf7ba43deb1d625f0697
Roman Gershman <romange@gmail.com>
2022-01-20 05:16:22 +0200

    Add HashCode support to CompactObject

--- core/compact_object_test.cc
+++ core/compact_object_test.cc
@@ -1,8 +1,10 @@
-// Copyright 2021, Roman Gershman.  All rights reserved.
+// Copyright 2022, Roman Gershman.  All rights reserved.
 // See LICENSE for licensing terms.
 //
-
 #include "core/compact_object.h"
+
+#include <xxhash.h>
+
 #include "base/gtest.h"
 
 extern "C" {
@@ -47,6 +49,15 @@ TEST_F(CompactObjectTest, Basic) {
   EXPECT_EQ(a, c);
 }
 
+TEST_F(CompactObjectTest, NonInline) {
+  string s(22, 'a');
+  CompactObj a{s};
+  XXH64_hash_t seed = 24061983;
+  uint64_t expected_val = XXH3_64bits_withSeed(s.data(), s.size(), seed);
+  EXPECT_EQ(18261733907982517826UL, expected_val);
+  EXPECT_EQ(expected_val, a.HashCode());
+}
+
 TEST_F(CompactObjectTest, Int) {
   cs_.SetString("0");
   EXPECT_EQ(0, cs_.TryGetInt());
@@ -54,6 +65,7 @@ TEST_F(CompactObjectTest, Int) {
   EXPECT_EQ("0", cs_.GetSlice(&tmp_));
   EXPECT_EQ(OBJ_STRING, cs_.ObjType());
   cs_.SetString("42");
+  EXPECT_EQ(8181779779123079347, cs_.HashCode());
   EXPECT_EQ(OBJ_ENCODING_INT, cs_.Encoding());
 }
 
