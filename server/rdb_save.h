751fc63ed5679c55bc6fb2be59f81f323fa746f1
Roman Gershman <romange@gmail.com>
2022-01-21 23:13:27 +0200

    Implement Save of rdb header/epilog

---
+++ server/rdb_save.h
@@ -0,0 +1,74 @@
+// Copyright 2022, Roman Gershman.  All rights reserved.
+// See LICENSE for licensing terms.
+//
+#pragma once
+
+extern "C" {
+#include "redis/lzfP.h"
+}
+
+#include "base/io_buf.h"
+#include "io/io.h"
+
+namespace dfly {
+class EngineShardSet;
+class EngineShard;
+
+
+class RdbSerializer {
+ public:
+  RdbSerializer(::io::Sink* s = nullptr);
+
+  ~RdbSerializer();
+
+  // The ownership stays with the caller.
+  void set_sink(::io::Sink* s) {
+    sink_ = s;
+  }
+
+  std::error_code WriteOpcode(uint8_t opcode) {
+    return WriteRaw(::io::Bytes{&opcode, 1});
+  }
+
+  std::error_code SaveKeyVal(std::string_view key, std::string_view value, uint64_t expire_ms);
+  std::error_code WriteRaw(const ::io::Bytes& buf);
+  std::error_code SaveString(std::string_view val);
+
+  std::error_code SaveString(const uint8_t* buf, size_t len) {
+    return SaveString(std::string_view{reinterpret_cast<const char*>(buf), len});
+  }
+
+  std::error_code SaveLen(size_t len);
+
+  std::error_code FlushMem();
+
+ private:
+  std::error_code SaveLzfBlob(const ::io::Bytes& src, size_t uncompressed_len);
+
+  ::io::Sink* sink_ = nullptr;
+  std::unique_ptr<LZF_HSLOT[]> lzf_;
+  base::IoBuf mem_buf_;
+  base::PODArray<uint8_t> tmp_buf_;
+};
+
+
+class RdbSaver {
+ public:
+  RdbSaver(EngineShardSet* ess, ::io::Sink* sink);
+  ~RdbSaver();
+
+  std::error_code SaveHeader();
+  std::error_code SaveEpilog();
+
+ private:
+  std::error_code SaveAux();
+  std::error_code SaveAuxFieldStrStr(std::string_view key, std::string_view val);
+  std::error_code SaveAuxFieldStrInt(std::string_view key, int64_t val);
+
+  EngineShardSet* ess_;
+
+  ::io::Sink* sink_;
+  RdbSerializer serializer_;
+};
+
+}  // namespace dfly
