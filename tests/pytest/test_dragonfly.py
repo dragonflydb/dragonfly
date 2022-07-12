d0bd8c8ee7688e632152f6e185e20e7f3341dfd2
Shmulik Klein <shmulik.klein@gmail.com>
2022-07-12 12:25:06 +0200

    test: lint using flake8
    Signed-off-by: Shmulik Klein <shmulik.klein@gmail.com>

--- tests/pytest/test_dragonfly.py
+++ tests/pytest/test_dragonfly.py
@@ -4,40 +4,43 @@ import os
 import subprocess
 import time
 from threading import Thread
-from redis.client import NEVER_DECODE
+
 
 @pytest.fixture(scope="module")
 def dragonfly_db():
     df_path = os.environ.get("DRAGONFLY_HOME", get_dragon_fly_default_path())
     print("Starting DragonflyDB [", df_path, "]")
-    #TODO: parse arguments and pass them over 
+    # TODO: parse arguments and pass them over
     p = subprocess.Popen([df_path])
     time.sleep(0.1)
     return_code = p.poll()
     if return_code is not None:
         pytest.exit("Failed to start DragonflyDB [{}]".format(df_path))
-    
+
     yield
 
     print("Terminating DragonflyDB process [", p.pid, "]")
     try:
         p.terminate()
         outs, errs = p.communicate(timeout=15)
-    except subprocess.TimeoutExpired as e:
+    except subprocess.TimeoutExpired:
         print("Unable to terminate DragonflyDB gracefully, it was killed")
         outs, errs = p.communicate()
         print(outs)
         print(errs)
 
+
 def get_dragon_fly_default_path():
     df_repo_root_dir = subprocess.Popen(['git', 'rev-parse', '--show-toplevel'], stdout=subprocess.PIPE).communicate()[0].rstrip().decode('utf-8')
     return df_repo_root_dir + '/build-opt/dragonfly'
+
 
 @pytest.fixture(scope="module")
 def client(dragonfly_db):
     pool = redis.ConnectionPool(decode_responses=True)
     client = redis.Redis(connection_pool=pool)
     return client
+
 
 class BLPopWorkerThread:
     def __init__(self):
@@ -58,8 +61,9 @@ class BLPopWorkerThread:
         self.thread.join(timeout)
         return not self.thread.is_alive()
 
+
 @pytest.mark.parametrize('index', range(50))
-def test_blpop_multiple_keys(client : redis.Redis, index):
+def test_blpop_multiple_keys(client: redis.Redis, index):
     wt_blpop = BLPopWorkerThread()
     wt_blpop.async_blpop(client)
 
