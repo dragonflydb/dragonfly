32fcb5f3156b3c8ead11e1445ba7066e88ce1768
Vladislav Oleshko <vlad@dragonflydb.io>
2022-10-24 23:24:23 +0300

    refactor(server): Refactor and tidy dflycmd
    Signed-off-by: Vladislav Oleshko <vlad@dragonflydb.io>

--- src/server/dflycmd.h
+++ src/server/dflycmd.h
@@ -8,6 +8,10 @@
 
 #include "server/conn_context.h"
 
+namespace facade {
+class RedisReplyBuilder;
+}  // namespace facade
+
 namespace util {
 class ListenerInterface;
 }  // namespace util
@@ -23,31 +27,61 @@ class Journal;
 
 class DflyCmd {
  public:
+  enum class SyncState { PREPARATION, FULL_SYNC };
+
+  struct SyncInfo {
+    SyncState state = SyncState::PREPARATION;
+
+    int64_t tx_id = 0;
+  };
+
+ public:
   DflyCmd(util::ListenerInterface* listener, ServerFamily* server_family);
 
   void Run(CmdArgList args, ConnectionContext* cntx);
 
-  uint32_t AllocateSyncSession();
-
   void OnClose(ConnectionContext* cntx);
 
-  // stops all background processes so we could exit in orderly manner.
+  // Stop all background processes so we can exit in orderly manner.
   void BreakOnShutdown();
 
+  // Create new sync session.
+  uint32_t CreateSyncSession();
+
  private:
-  void HandleJournal(CmdArgList args, ConnectionContext* cntx);
+  // JOURNAL [START/STOP]
+  // Start or stop journaling.
+  void Journal(CmdArgList args, ConnectionContext* cntx);
+
+  // THREAD [to_thread]
+  // Return connection thread index or migrate to another thread.
+  void Thread(CmdArgList args, ConnectionContext* cntx);
+
+  // SYNC <masterid> <syncid> <flowid>
+  // Migrate connection to required flow thread.
+  // Stub: will be replcaed with full sync.
+  void Sync(CmdArgList args, ConnectionContext* cntx);
+
+  // EXPIRE
+  // Check all keys for expiry.
+  void Expire(CmdArgList args, ConnectionContext* cntx);
+
+  // Delete sync session.
+  void DeleteSyncSession(uint32_t sync_id);
+
+  // Find sync info by id or send error reply.
+  std::pair<uint32_t, SyncInfo*> GetSyncInfoOrReply(std::string_view id,
+                                                    facade::RedisReplyBuilder* rb);
+
+  ServerFamily* sf_;
 
   util::ListenerInterface* listener_;
-  ServerFamily* sf_;
-  ::boost::fibers::mutex mu_;
   TxId journal_txid_ = 0;
 
-  struct SyncInfo {
-    int64_t tx_id = 0;
-  };
-
-  absl::btree_map<uint32_t, SyncInfo> sync_info_;
+  absl::btree_map<uint32_t, SyncInfo*> sync_infos_;
   uint32_t next_sync_id_ = 1;
+
+  ::boost::fibers::mutex mu_;  // guard sync info and journal operations.
 };
 
 }  // namespace dfly
