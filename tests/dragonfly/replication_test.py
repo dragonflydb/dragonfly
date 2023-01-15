9221c03d567c2e5ab13d5ad6a04314fefb1a0e13
Vladislav Oleshko <vlad@dragonflydb.io>
2023-01-15 20:45:43 +0300

    fix(tests): Fix tests after merge
    Signed-off-by: Vladislav Oleshko <vlad@dragonflydb.io>

--- tests/dragonfly/replication_test.py
+++ tests/dragonfly/replication_test.py
@@ -317,33 +317,16 @@ async def test_disconnect_master(df_local_factory, df_seeder_factory, t_master, 
         assert await seeder.compare(capture, port=replica.port)
 
 
-
-"""
-Test key expiration. Test propagation from both eviction on access and garbage collection.
-"""
-
-expiration_cases = [
-    (100)
-]
-
-
-@pytest.mark.asyncio
-@pytest.mark.parametrize("n_keys", expiration_cases)
-async def test_expiration(df_local_factory, n_keys):
-    master = df_local_factory.create(port=BASE_PORT, proactor_threads=4)
-    replica = df_local_factory.create(port=BASE_PORT+1, proactor_threads=4)
-
-
 """
 Test flushall command. Set data to master send flashall and set more data.
 Check replica keys at the end.
 """
 
+
 @pytest.mark.asyncio
 async def test_flushall(df_local_factory):
     master = df_local_factory.create(port=BASE_PORT, proactor_threads=4)
     replica = df_local_factory.create(port=BASE_PORT+1, proactor_threads=2)
-
 
     master.start()
     replica.start()
@@ -352,14 +335,8 @@ async def test_flushall(df_local_factory):
     c_replica = aioredis.Redis(port=replica.port)
     await c_replica.execute_command(f"REPLICAOF localhost {master.port}")
 
-
-    # Set simple keys 1..n_keys on master with 100 second expire
-    c_master = aioredis.Redis(port=master.port)
-    pipe = c_master.pipeline(transaction=False)
-    for i in range(n_keys):
-        pipe.set(f"{i}", "v", ex=100)
-
     n_keys = 1000
+
     def gen_test_data(start, end):
         for i in range(start, end):
             yield f"key-{i}", f"value-{i}"
@@ -371,14 +348,53 @@ async def test_flushall(df_local_factory):
     # flushall
     pipe.flushall()
     # Set simple keys n_keys..n_keys*2 on master
-    batch_fill_data(client=pipe, gen=gen_test_data(n_keys, n_keys*2), batch_size=3)
-
+    batch_fill_data(client=pipe, gen=gen_test_data(
+        n_keys, n_keys*2), batch_size=3)
 
     await pipe.execute()
 
     # Wait for replica to receive them
     await asyncio.sleep(1)
 
+    # Check replica keys 0..n_keys-1 dont exist
+    pipe = c_replica.pipeline(transaction=False)
+    for i in range(n_keys):
+        pipe.get(f"key-{i}")
+    vals = await pipe.execute()
+    assert all(v is None for v in vals)
+
+    # Check replica keys n_keys..n_keys*2-1 exist
+    for i in range(n_keys, n_keys*2):
+        pipe.get(f"key-{i}")
+    vals = await pipe.execute()
+    assert all(v is not None for v in vals)
+
+
+"""
+Test key expiration. Test propagation from both eviction on access and garbage collection.
+"""
+
+
+@pytest.mark.asyncio
+async def test_expiration(df_local_factory, n_keys=500):
+    master = df_local_factory.create(port=BASE_PORT, proactor_threads=1)
+    replica = df_local_factory.create(port=BASE_PORT+1, proactor_threads=1)
+    master.start()
+    replica.start()
+
+    # Connect replica to master
+    c_replica = aioredis.Redis(port=replica.port)
+    await c_replica.execute_command(f"REPLICAOF localhost {master.port}")
+
+    # Set simple keys 1..n_keys on master with 100 second expire
+    c_master = aioredis.Redis(port=master.port)
+    pipe = c_master.pipeline(transaction=False)
+    for i in range(n_keys):
+        pipe.set(f"{i}", "v", ex=100)
+    await pipe.execute()
+
+    # Wait for replica to receive them
+    await asyncio.sleep(1)
 
     # Check replica has them and their ttl are set
     pipe = c_replica.pipeline(transaction=False)
@@ -417,17 +433,3 @@ async def test_flushall(df_local_factory):
     # Check odd keys expired on replica
     vals = await c_replica.mget(f"{i}" for i in range(n_keys))
     assert all(v is None for v in vals)
-
-    # Check replica keys 0..n_keys-1 dont exist
-    pipe = c_replica.pipeline(transaction=False)
-    for i in range(n_keys):
-        pipe.get(f"key-{i}")
-    vals = await pipe.execute()
-    assert all(v is None for v in vals)
-
-    # Check replica keys n_keys..n_keys*2-1 exist
-    for i in range(n_keys, n_keys*2):
-        pipe.get(f"key-{i}")
-    vals = await pipe.execute()
-    assert all(v is not None for v in vals)
-
