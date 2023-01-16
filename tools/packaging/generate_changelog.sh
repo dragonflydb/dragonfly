ec0ac23f82e10df7e953372b1cd905e10106c702
Boaz Sade <boaz@dragonflydb.io>
2023-01-16 18:27:03 +0200

    feat(actions): add support for debian packaging
    Signed-off-by: Boaz Sade <boaz@dragonflydb.io>

--- tools/packaging/generate_changelog.sh
+++ tools/packaging/generate_changelog.sh
@@ -18,9 +18,16 @@ THIS_DIR=$(dirname ${SCRIPT_ABS_PATH})
 GIT_DIR=$1
 PACKGE_DIR=${THIS_DIR}/debian
 CHANGE_LOG=${PACKGE_DIR}/changelog
-
 cd ${GIT_DIR}
 git config --global --add safe.directory ${GIT_DIR}
+has_tags=$(git tag -l v* | wc -l 2>/dev/null)
+if [ "$has_tags" = "" -o "$has_tags" = "0" ]; then
+	git fetch --all --tags || {
+		echo "failed to fetch tags, cannot build changelog file"
+		exit 1
+	}
+fi
+
 >${CHANGE_LOG}
 prevtag=v0.2.0
 pkgname=`cat ${PACKGE_DIR}/control | grep '^Package: ' | sed 's/^Package: //'`
@@ -28,3 +35,13 @@ git tag -l v* | sort -V | while read tag; do
     (echo "$pkgname (${tag#v}) unstable; urgency=low"; git log --pretty=format:'  * %s' $prevtag..$tag; git log --pretty='format:%n%n -- %aN <%aE>  %aD%n%n' $tag^..$tag) | cat - ${CHANGE_LOG} | sponge ${CHANGE_LOG}
         prevtag=$tag
 done
+if [ -f ${CHANGE_LOG} ]; then
+	haslnes=$(wc -l ${CHANGE_LOG} 2>/dev/null | awk '{print $1}')
+	if [ "$haslnes" = "" ]; then
+		echo "empty file ${CHANGE_LOG}, failed to generate changelog"
+		exit 1
+	fi
+else
+	echo "failed to generate ${CHANGE_LOG}"
+	exit 1
+fi
