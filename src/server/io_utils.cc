604446351baa2c07f2611cad35a57060049b0a41
adi_holden <adi@dragonflydb.io>
2023-02-05 15:21:26 +0200

    bug(server): fix deadlock in BufferedStreamerBase notify all producers
    Signed-off-by: adi_holden <adi@dragonflydb.io>

--- src/server/io_utils.cc
+++ src/server/io_utils.cc
@@ -30,7 +30,6 @@ error_code BufferedStreamerBase::ConsumeIntoSink(io::Sink* dest) {
   while (!IsStopped()) {
     // Wait for more data or stop signal.
     waker_.await([this]() { return buffered_ > 0 || IsStopped(); });
-
     // Break immediately on cancellation.
     if (cll_->IsCancelled()) {
       waker_.notifyAll();  // Wake consumer if it missed it.
@@ -42,7 +41,7 @@ error_code BufferedStreamerBase::ConsumeIntoSink(io::Sink* dest) {
     buffered_ = 0;
 
     // If producer stalled, notify we consumed data and it can unblock.
-    waker_.notify();
+    waker_.notifyAll();
 
     // Write data and check for errors.
     if (auto ec = dest->Write(consumer_buf_.InputBuffer()); ec) {
