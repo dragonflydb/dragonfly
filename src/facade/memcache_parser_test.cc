9c9ae5d9a945b3a7bf33475530643fd1831d1c94
talbii <ido@dragonflydb.io>
2023-05-17 16:03:22 +0300

    introduce more tests, along with unit testing
    Signed-off-by: talbii <ido@dragonflydb.io>

--- src/facade/memcache_parser_test.cc
+++ src/facade/memcache_parser_test.cc
@@ -78,4 +78,54 @@ TEST_F(MCParserTest, Stats) {
   EXPECT_EQ(MemcacheParser::PARSE_ERROR, st);
 }
 
+TEST_F(MCParserTest, NoreplyBasic) {
+  MemcacheParser::Result st = parser_.Parse("set mykey 1 2 3 noreply\r\n", &consumed_, &cmd_);
+
+  EXPECT_EQ(MemcacheParser::OK, st);
+  EXPECT_EQ("mykey", cmd_.key);
+  EXPECT_EQ(1, cmd_.flags);
+  EXPECT_EQ(2, cmd_.expire_ts);
+  EXPECT_EQ(3, cmd_.bytes_len);
+  EXPECT_EQ(MemcacheParser::SET, cmd_.type);
+  EXPECT_TRUE(cmd_.no_reply);
+
+  cmd_ = MemcacheParser::Command{};
+  st = parser_.Parse("set mykey2 4 5 6\r\n", &consumed_, &cmd_);
+
+  EXPECT_EQ(MemcacheParser::OK, st);
+  EXPECT_EQ("mykey2", cmd_.key);
+  EXPECT_EQ(4, cmd_.flags);
+  EXPECT_EQ(5, cmd_.expire_ts);
+  EXPECT_EQ(6, cmd_.bytes_len);
+  EXPECT_EQ(MemcacheParser::SET, cmd_.type);
+  EXPECT_FALSE(cmd_.no_reply);
+}
+
+class MCParserNoreplyTest : public MCParserTest {
+ protected:
+  void RunTest(string_view str, bool noreply) {
+    MemcacheParser::Result st = parser_.Parse(str, &consumed_, &cmd_);
+
+    EXPECT_EQ(MemcacheParser::OK, st);
+    EXPECT_EQ(cmd_.no_reply, noreply);
+  }
+};
+
+TEST_F(MCParserNoreplyTest, StoreCommands) {
+  RunTest("set mykey 0 0 3 noreply\r\n", true);
+  RunTest("set mykey 0 0 3\r\n", false);
+  RunTest("add mykey 0 0 3\r\n", false);
+  RunTest("replace mykey 0 0 3\r\n", false);
+  RunTest("append mykey 0 0 3\r\n", false);
+  RunTest("prepend mykey 0 0 3\r\n", false);
+}
+
+TEST_F(MCParserNoreplyTest, Other) {
+  RunTest("quit\r\n", false);
+  RunTest("delete mykey\r\n", false);
+  RunTest("incr mykey 1\r\n", false);
+  RunTest("decr mykey 1\r\n", false);
+  RunTest("flush_all\r\n", false);
+}
+
 }  // namespace facade
--- tests/dragonfly/pymemcached_test.py
+++ tests/dragonfly/pymemcached_test.py
@@ -17,6 +17,16 @@ def test_add_set(memcached_connection):
 @dfly_args({"memcache_port": 11211})
 def test_set_add(memcached_connection):
     memcached_connection.set(b"key", b"data")
+    # stuck here
     assert not memcached_connection.add(b"key", b"other", noreply=False)
+    # expects to see NOT_STORED
     memcached_connection.set(b"key", b"other")
     assert memcached_connection.get(b"key") == b"other"
+
+@dfly_args({"memcache_port": 11211})
+def test_mixed_reply(memcached_connection):
+    memcached_connection.set(b"key", b"data", noreply=True)
+    memcached_connection.add(b"key", b"other", noreply=False)
+    memcached_connection.add(b"key", b"final", noreply=True)
+
+    assert memcached_connection.get(b"key") == b"data"
