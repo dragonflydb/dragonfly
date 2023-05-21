c0190943ad1dc2f6d2bf0da50e514dfcd1b2930e
Roy Jacobson <roy@dragonflydb.io>
2023-05-21 13:20:10 +0300

    Small fixes/warnings

--- src/core/search/base.h
+++ src/core/search/base.h
@@ -17,6 +17,7 @@ namespace dfly::search {
 struct DocumentAccessor {
   // Callback that's supplied with field values.
   using FieldConsumer = std::function<bool(std::string_view)>;
+  virtual ~DocumentAccessor() = default;
 
   virtual bool Check(FieldConsumer f, std::string_view active_field) const = 0;
 };
