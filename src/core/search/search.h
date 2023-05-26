b20ffd51118adee6dcd9d17999d43b5f2e66d119
Vladislav Oleshko <vlad@dragonflydb.io>
2023-05-26 21:00:13 +0300

    chore: remove search/base
    Signed-off-by: Vladislav Oleshko <vlad@dragonflydb.io>

--- src/core/search/search.h
+++ src/core/search/search.h
@@ -4,11 +4,23 @@
 
 #pragma once
 
+#include <functional>
+#include <string>
+
 #include "core/search/base.h"
 
 namespace dfly::search {
 
 struct AstNode;
+
+// Interface for accessing document values with different data structures underneath.
+struct DocumentAccessor {
+  // Callback that's supplied with field values.
+  using FieldConsumer = std::function<bool(std::string_view)>;
+  virtual ~DocumentAccessor() = default;
+
+  virtual bool Check(FieldConsumer f, std::string_view active_field) const = 0;
+};
 
 class SearchAlgorithm {
  public:
