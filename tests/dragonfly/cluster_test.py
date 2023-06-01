67ca8c8615df670a8f968837c1162710ec66511e
Chaka <chakaz@users.noreply.github.com>
2023-06-01 11:46:52 +0300

    test(cluster): Add basic cluster mode pytests. (#1328)

--- tests/dragonfly/cluster_test.py
+++ tests/dragonfly/cluster_test.py
@@ -8,6 +8,18 @@ from . import dfly_args
 BASE_PORT = 30001
 
 
+async def push_config(config, admin_connections):
+    print("Pushing config ", config)
+    await asyncio.gather(*(c_admin.execute_command(
+        "DFLYCLUSTER", "CONFIG", config)
+        for c_admin in admin_connections))
+
+
+async def get_node_id(admin_connection):
+    id = await admin_connection.execute_command("DFLYCLUSTER MYID")
+    return id.decode()
+
+
 @dfly_args({})
 class TestNotEmulated:
     async def test_cluster_commands_fails_when_not_emulate(self, async_client: aioredis.Redis):
@@ -142,3 +154,133 @@ async def test_cluster_nodes(async_client):
     assert info['last_ping_sent'] == '0'
     assert info['slots'] == [['0', '16383']]
     assert info['master_id'] == "-"
+
+
+"""
+Test that slot ownership changes correctly with config changes.
+
+Add a key to node0, then move the slot ownership to node1 and see that they both behave as
+intended.
+Also add keys to each of them that are *not* moved, and see that they are unaffected by the move.
+"""
+@dfly_args({"proactor_threads": 4, "cluster_mode": "yes"})
+async def test_cluster_slot_ownership_changes(df_local_factory):
+    # Start and configure cluster with 2 nodes
+    nodes = [
+        df_local_factory.create(port=BASE_PORT+i, admin_port=BASE_PORT+i+1000)
+        for i in range(2)
+    ]
+
+    df_local_factory.start_all(nodes)
+
+    c_nodes = [aioredis.Redis(port=node.port) for node in nodes]
+    c_nodes_admin = [aioredis.Redis(port=node.admin_port) for node in nodes]
+
+    node_ids = await asyncio.gather(*(get_node_id(c) for c in c_nodes_admin))
+
+    config = f"""
+      [
+        {{
+          "slot_ranges": [
+            {{
+              "start": 0,
+              "end": 5259
+            }}
+          ],
+          "master": {{
+            "id": "{node_ids[0]}",
+            "ip": "localhost",
+            "port": {nodes[0].port}
+          }},
+          "replicas": []
+        }},
+        {{
+          "slot_ranges": [
+            {{
+              "start": 5260,
+              "end": 16383
+            }}
+          ],
+          "master": {{
+            "id": "{node_ids[1]}",
+            "ip": "localhost",
+            "port": {nodes[1].port}
+          }},
+          "replicas": []
+        }}
+      ]
+    """
+
+    await push_config(config, c_nodes_admin)
+
+    # Slot for "KEY1" is 5259
+
+    # Insert a key that should stay in node0
+    assert await c_nodes[0].set("KEY0", "value")
+
+    # And to node1 (so it happens that 'KEY0' belongs to 0 and 'KEY2' to 1)
+    assert await c_nodes[1].set("KEY2", "value")
+
+    # Insert a key that we will move ownership of to node1 (but without migration yet)
+    assert await c_nodes[0].set("KEY1", "value")
+    assert await c_nodes[0].execute_command("DBSIZE") == 2
+
+    # Make sure that node0 owns "KEY0"
+    assert (await c_nodes[0].get("KEY0")).decode() == "value"
+
+    # Make sure that "KEY1" is not owned by node1
+    try:
+        await c_nodes[1].set("KEY1", "value")
+        assert False, "Should not be able to set key on non-owner cluster node"
+    except redis.exceptions.ResponseError as e:
+        assert e.args[0] == "MOVED 5259 localhost:30001"
+
+    # And that node1 only has 1 key ("KEY2")
+    assert await c_nodes[1].execute_command("DBSIZE") == 1
+
+    print("Moving ownership over 5259 ('KEY1') to other node")
+
+    config = config.replace('5259', '5258').replace('5260', '5259')
+    await push_config(config, c_nodes_admin)
+
+    # node0 should have removed "KEY1" as it no longer owns it
+    assert await c_nodes[0].execute_command("DBSIZE") == 1
+    # node0 should still own "KEY0" though
+    assert (await c_nodes[0].get("KEY0")).decode() == "value"
+    # node1 should still have "KEY2"
+    assert await c_nodes[1].execute_command("DBSIZE") == 1
+
+    # Now node0 should reply with MOVED for "KEY1"
+    try:
+        await c_nodes[0].set("KEY1", "value")
+        assert False, "Should not be able to set key on non-owner cluster node"
+    except redis.exceptions.ResponseError as e:
+        assert e.args[0] == "MOVED 5259 localhost:30002"
+
+    # And node1 should own it and allow using it
+    assert await c_nodes[1].set("KEY1", "value")
+    assert await c_nodes[1].execute_command("DBSIZE") == 2
+
+    config = f"""
+      [
+        {{
+          "slot_ranges": [
+            {{
+              "start": 0,
+              "end": 16383
+            }}
+          ],
+          "master": {{
+            "id": "{node_ids[0]}",
+            "ip": "localhost",
+            "port": {nodes[0].port}
+          }},
+          "replicas": []
+        }}
+      ]
+    """
+    await push_config(config, c_nodes_admin)
+
+    assert await c_nodes[0].execute_command("DBSIZE") == 1
+    assert (await c_nodes[0].get("KEY0")).decode() == "value"
+    assert await c_nodes[1].execute_command("DBSIZE") == 0
