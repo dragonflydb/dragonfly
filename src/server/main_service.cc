6b91fb17cdc161f3ef9c742e42b30f653cfdb540
Roy Jacobson <roy@dragonflydb.io>
2023-06-27 15:31:44 +0200

    use std move

--- src/server/main_service.cc
+++ src/server/main_service.cc
@@ -557,7 +557,7 @@ void Service::Init(util::AcceptServer* acceptor, std::vector<facade::Listener*> 
   request_latency_usec.Init(&pp_);
   StringFamily::Init(&pp_);
   GenericFamily::Init(&pp_);
-  server_family_.Init(acceptor, listeners, &cluster_family_);
+  server_family_.Init(acceptor, std::move(listeners), &cluster_family_);
 
   ChannelStore* cs = new ChannelStore{};
   pp_.Await(
