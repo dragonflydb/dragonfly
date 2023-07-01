54fbf0d6fcecc814abd61515b4eacb5064263447
Vladislav <vlad@dragonflydb.io>
2023-07-02 01:02:18 +0300

    feat: compressed sorted set (#1474)
    Implements basic compressed sorted set that can be used in indices to optimize memory usage
    
    ---------
    
    Signed-off-by: Vladislav Oleshko <vlad@dragonflydb.io>

---
+++ src/core/search/compressed_sorted_set.h
@@ -0,0 +1,113 @@
+#pragma once
+
+#include <absl/types/span.h>
+
+#include <cstdint>
+#include <iterator>
+#include <optional>
+#include <vector>
+
+#include "core/search/base.h"
+
+namespace dfly::search {
+
+// A list of sorted unique integers with reduced memory usage.
+// Only differences between successive elements are stored
+// in a variable length encoding.
+class CompressedSortedSet {
+ public:
+  using IntType = DocId;
+
+  // Const access iterator that decodes the compressed list on traversal
+  struct ConstIterator {
+    friend class CompressedSortedSet;
+
+    // To make it work with std container contructors
+    using iterator_category = std::forward_iterator_tag;
+    using difference_type = std::ptrdiff_t;
+    using value_type = IntType;
+    using pointer = IntType*;
+    using reference = IntType&;
+
+    IntType operator*() const;
+    ConstIterator& operator++();
+
+    friend class CompressedSortedSet;
+    friend bool operator==(const ConstIterator& l, const ConstIterator& r);
+    friend bool operator!=(const ConstIterator& l, const ConstIterator& r);
+
+   private:
+    explicit ConstIterator(const CompressedSortedSet& list);
+    ConstIterator() = default;
+
+    void ReadNext();  // Decode next value to stash
+
+    std::optional<IntType> stash_{};
+    absl::Span<const uint8_t> last_read_{};
+    absl::Span<const uint8_t> diffs_{};
+  };
+
+  // Output iterator to build compressed list from sorted range
+  struct SortedBackInserter {
+    using iterator_category = std::forward_iterator_tag;
+    using difference_type = std::ptrdiff_t;
+    using value_type = IntType;
+    using pointer = IntType*;
+    using reference = IntType&;
+
+    explicit SortedBackInserter(CompressedSortedSet* list);
+
+    SortedBackInserter& operator*() {
+      return *this;
+    }
+    SortedBackInserter& operator++() {
+      return *this;
+    }
+
+    SortedBackInserter& operator=(IntType value);
+
+   private:
+    IntType last_;
+    CompressedSortedSet* list_;
+  };
+
+  friend struct Iterator;
+  friend struct SortedBackInserter;
+
+ public:
+  ConstIterator begin() const;
+  ConstIterator end() const;
+
+  void Insert(IntType value);  // Insert arbitrary element, needs to scan whole list
+  void Remove(IntType value);  // Remove arbitrary element, needs to scan whole list
+
+  size_t Size() const;
+  size_t ByteSize() const;
+
+ private:
+  struct EntryLocation {
+    IntType value;                        // Value or 0
+    IntType prev_value;                   // Preceding value or 0
+    absl::Span<const uint8_t> diff_span;  // Location of value encoded diff, empty if none read
+  };
+
+ private:
+  // Find EntryLocation of first entry that is not less than value (std::lower_bound)
+  EntryLocation LowerBound(IntType value) const;
+
+  // Push back difference without any decoding. Used only for efficient construction from sorted
+  // list
+  void PushBackDiff(IntType diff);
+
+  // Encode integer with variable length encoding into buf and return written subspan
+  static absl::Span<uint8_t> WriteVarLen(IntType value, absl::Span<uint8_t> buf);
+
+  // Decode integer with variable length encoding from source
+  static std::pair<IntType /*value*/, size_t /*read*/> ReadVarLen(absl::Span<const uint8_t> source);
+
+ private:
+  IntType size_{0};
+  std::vector<uint8_t> diffs_{};
+};
+
+}  // namespace dfly::search
