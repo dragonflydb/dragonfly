9698d6fea24f1cf1f2631c9c076a239b43362cad
Kostas Kyrimis <kostas@dragonflydb.io>
2023-07-20 14:35:41 +0300

    test(replication): add admin port replication tests (#1561)

--- tests/dragonfly/replication_test.py
+++ tests/dragonfly/replication_test.py
@@ -23,24 +23,31 @@ Test full replication pipeline. Test full sync with streaming changes and stable
 # 1. Number of master threads
 # 2. Number of threads for each replica
 # 3. Seeder config
+# 4. Admin port
 replication_cases = [
-    (8, [8], dict(keys=10_000, dbcount=4)),
-    (6, [6, 6, 6], dict(keys=4_000, dbcount=4)),
-    (8, [2, 2, 2, 2], dict(keys=4_000, dbcount=4)),
-    (4, [8, 8], dict(keys=4_000, dbcount=4)),
-    (4, [1] * 8, dict(keys=500, dbcount=2)),
-    (1, [1], dict(keys=100, dbcount=2)),
+    (8, [8], dict(keys=10_000, dbcount=4), False),
+    (6, [6, 6, 6], dict(keys=4_000, dbcount=4), False),
+    (8, [2, 2, 2, 2], dict(keys=4_000, dbcount=4), False),
+    (4, [8, 8], dict(keys=4_000, dbcount=4), False),
+    (4, [1] * 8, dict(keys=500, dbcount=2), False),
+    (1, [1], dict(keys=100, dbcount=2), False),
+    (6, [6, 6, 6], dict(keys=500, dbcount=4), True),
+    (1, [1], dict(keys=100, dbcount=2), True),
 ]
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize("t_master, t_replicas, seeder_config", replication_cases)
+@pytest.mark.parametrize("t_master, t_replicas, seeder_config, from_admin_port", replication_cases)
 async def test_replication_all(
-    df_local_factory, df_seeder_factory, t_master, t_replicas, seeder_config
+    df_local_factory, df_seeder_factory, t_master, t_replicas, seeder_config, from_admin_port
 ):
-    master = df_local_factory.create(port=BASE_PORT, proactor_threads=t_master)
+    master = df_local_factory.create(
+        port=BASE_PORT, admin_port=ADMIN_PORT, proactor_threads=t_master
+    )
     replicas = [
-        df_local_factory.create(port=BASE_PORT + i + 1, proactor_threads=t)
+        df_local_factory.create(
+            port=BASE_PORT + i + 1, admin_port=ADMIN_PORT + i + 1, proactor_threads=t
+        )
         for i, t in enumerate(t_replicas)
     ]
 
@@ -62,8 +69,10 @@ async def test_replication_all(
     await asyncio.sleep(0.0)
 
     # Start replication
+    master_port = master.port if not from_admin_port else master.admin_port
+
     async def run_replication(c_replica):
-        await c_replica.execute_command("REPLICAOF localhost " + str(master.port))
+        await c_replica.execute_command("REPLICAOF localhost " + str(master_port))
 
     await asyncio.gather(*(asyncio.create_task(run_replication(c)) for c in c_replicas))
 
