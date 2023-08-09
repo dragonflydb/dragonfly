44ef1d5456c7b42241f7663729c7a01dd43e91f5
Vladislav Oleshko <vlad@dragonflydb.io>
2023-08-09 09:50:56 +0300

    fix: fix merge
    Signed-off-by: Vladislav Oleshko <vlad@dragonflydb.io>

--- src/facade/dragonfly_connection.cc
+++ src/facade/dragonfly_connection.cc
@@ -887,7 +887,8 @@ void Connection::SendAsync(MessageHandle msg) {
   if (!dispatch_fb_.IsJoinable()) {
     DCHECK_EQ(dispatch_q_.size(), 0u);
     auto* peer = socket_.get();
-    dispatch_fb_ = MakeFiber(dfly::Launch::post, "connection_dispatch", [&] { DispatchFiber(peer); });
+    dispatch_fb_ =
+        fb2::Fiber(dfly::Launch::post, "connection_dispatch", [&] { DispatchFiber(peer); });
   }
 
   dispatch_q_bytes_.fetch_add(msg.UsedMemory(), memory_order_relaxed);
