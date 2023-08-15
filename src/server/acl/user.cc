773cee12f49be8f24171dc7cd3691d657f1d9f94
Roy Jacobson <roy@dragonflydb.io>
2023-08-15 17:28:50 +0200

    security: Fix the password hashing in UserRegistry. (#1702)

--- src/server/acl/user.cc
+++ src/server/acl/user.cc
@@ -4,9 +4,20 @@
 
 #include "server/acl/user.h"
 
-#include <xxhash.h>
+#include <openssl/sha.h>
 
 namespace dfly {
+
+namespace {
+std::string StringSHA256(std::string_view password) {
+  std::string hash;
+  hash.resize(SHA256_DIGEST_LENGTH);
+  SHA256(reinterpret_cast<const unsigned char*>(password.data()), password.size(),
+         reinterpret_cast<unsigned char*>(hash.data()));
+  return hash;
+}
+
+}  // namespace
 
 User::User() {
   // acl_categories_ = AclCat::ACL_CATEGORY_ADMIN;
@@ -14,7 +25,7 @@ User::User() {
 
 void User::Update(UpdateRequest&& req) {
   if (req.password) {
-    SetPassword(*req.password);
+    SetPasswordHash(*req.password);
   }
 
   if (req.plus_acl_categories) {
@@ -30,19 +41,19 @@ void User::Update(UpdateRequest&& req) {
   }
 }
 
-void User::SetPassword(std::string_view password) {
-  password_ = HashPassword(password);
+void User::SetPasswordHash(std::string_view password) {
+  password_hash_ = StringSHA256(password);
 }
 
 bool User::HasPassword(std::string_view password) const {
-  if (!password_) {
+  if (!password_hash_) {
     if (password == "nopass") {
       return true;
     }
     return false;
   }
   // hash password and compare
-  return *password_ == HashPassword(password);
+  return *password_hash_ == StringSHA256(password);
 }
 
 void User::SetAclCategories(uint64_t cat) {
@@ -68,10 +79,6 @@ void User::SetIsActive(bool is_active) {
 
 bool User::IsActive() const {
   return is_active_;
-}
-
-uint32_t User::HashPassword(std::string_view password) const {
-  return XXH3_64bits(password.data(), password.size());
 }
 
 }  // namespace dfly
