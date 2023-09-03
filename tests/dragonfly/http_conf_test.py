683a8207c51f901f81a65c89d737c472c4f970d3
Roy Jacobson <roy@dragonflydb.io>
2023-09-03 14:56:39 +0300

    Enable authentication for the HTTP interface.

---
+++ tests/dragonfly/http_conf_test.py
@@ -0,0 +1,54 @@
+import aiohttp
+
+
+async def test_password(df_factory):
+    # Needs a private key and certificate.
+    server = df_factory.create(port=1112, requirepass="XXX")
+    server.start()
+
+    async with aiohttp.ClientSession() as session:
+        resp = await session.get(f"http://localhost:{server.port}/")
+        assert resp.status == 401
+    async with aiohttp.ClientSession(auth=aiohttp.BasicAuth("user", "wrongpassword")) as session:
+        resp = await session.get(f"http://localhost:{server.port}/")
+        assert resp.status == 401
+    async with aiohttp.ClientSession(auth=aiohttp.BasicAuth("user", "XXX")) as session:
+        resp = await session.get(f"http://localhost:{server.port}/")
+        assert resp.status == 200
+    server.stop()
+
+
+async def test_no_password_on_admin(df_factory):
+    # Needs a private key and certificate.
+    server = df_factory.create(
+        port=1112,
+        admin_port=1113,
+        requirepass="XXX",
+        noprimary_port_http_enabled=None,
+        admin_nopass=None,
+    )
+    server.start()
+
+    async with aiohttp.ClientSession(auth=aiohttp.BasicAuth("user", "XXX")) as session:
+        resp = await session.get(f"http://localhost:{server.admin_port}/")
+        assert resp.status == 200
+    server.stop()
+
+
+async def test_password_on_admin(df_factory):
+    # Needs a private key and certificate.
+    server = df_factory.create(
+        port=1112,
+        admin_port=1113,
+        requirepass="XXX",
+        admin_nopass=None,
+    )
+    server.start()
+
+    async with aiohttp.ClientSession(auth=aiohttp.BasicAuth("user", "badpass")) as session:
+        resp = await session.get(f"http://localhost:{server.port}/")
+        assert resp.status == 401
+    async with aiohttp.ClientSession(auth=aiohttp.BasicAuth("user", "XXX")) as session:
+        resp = await session.get(f"http://localhost:{server.port}/")
+        assert resp.status == 200
+    server.stop()
