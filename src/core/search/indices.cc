e0af5fe83615b5af28eb2b0ad0cdc00f846a1b8d
Vladislav <vlad@dragonflydb.io>
2023-09-06 15:06:38 +0300

    Remove ICU library (#1812)
    * chore(search): Replace icu with unialgo
    
    ---------
    
    Signed-off-by: Vladislav Oleshko <vlad@dragonflydb.io>

--- src/core/search/indices.cc
+++ src/core/search/indices.cc
@@ -8,12 +8,14 @@
 #include <absl/strings/ascii.h>
 #include <absl/strings/numbers.h>
 #include <absl/strings/str_split.h>
-#include <unicode/brkiter.h>
-#include <unicode/unistr.h>
+
+#define UNI_ALGO_DISABLE_NFKC_NFKD
+
+#include <uni_algo/case.h>
+#include <uni_algo/ranges_word.h>
 
 #include <algorithm>
 #include <cctype>
-#include <regex>
 
 #include "base/logging.h"
 
@@ -28,58 +30,11 @@ bool IsAllAscii(string_view sv) {
 }
 
 // Get all words from text as matched by the ICU library
-absl::flat_hash_set<std::string> ICUTokenizeWords(std::string_view text) {
-  // Is text contains only ascii, skip working with ICU resources
-  if (IsAllAscii(text)) {
-    std::regex rx{"\\b.*?\\b", std::regex_constants::icase};
-    std::cregex_iterator begin{text.data(), text.data() + text.size(), rx}, end{};
-
-    absl::flat_hash_set<string> words;
-    for (auto it = begin; it != end; ++it) {
-      auto word = it->str();
-      absl::AsciiStrToLower(&word);
-      words.insert(move(word));
-    }
-    return words;
-  }
-
-  icu::UnicodeString uStr(text.data(), text.size(), "UTF-8");
-
-  UErrorCode status = U_ZERO_ERROR;
-  std::unique_ptr<icu::BreakIterator> wordIter{
-      icu::BreakIterator::createWordInstance(icu::Locale::getDefault(), status)};
-
-  if (U_FAILURE(status))
-    return {};
-
-  wordIter->setText(uStr);
-
-  std::string tmpStdWord;
+absl::flat_hash_set<std::string> TokenizeWords(std::string_view text) {
   absl::flat_hash_set<std::string> words;
-
-  int32_t start = wordIter->first();
-  for (int32_t end = wordIter->next(); end != icu::BreakIterator::DONE;
-       start = end, end = wordIter->next()) {
-    icu::UnicodeString word = uStr.tempSubStringBetween(start, end);
-    // If the substring is not a space, convert it to lowercase and add to results
-    if (!word.isBogus() && !word.trim().isEmpty()) {
-      word.toLower();
-      word.toUTF8String(tmpStdWord);
-      words.emplace(move(tmpStdWord));
-    }
-  }
-
+  for (std::string_view word : una::views::word_only::utf8(text))
+    words.insert(una::cases::to_lowercase_utf8(word));
   return words;
-}
-
-// Convert string to lowercase with ICU library
-std::string ICUToLowercase(string_view input) {
-  icu::UnicodeString uStr =
-      icu::UnicodeString::fromUTF8(icu::StringPiece(input.data(), input.size()));
-  uStr.toLower();
-  std::string result;
-  uStr.toUTF8String(result);
-  return result;
 }
 
 // Split taglist, remove duplicates and convert all to lowercase
@@ -127,7 +82,7 @@ const CompressedSortedSet* BaseStringIndex::Matching(string_view str) const {
   if (IsAllAscii(str))
     word = absl::AsciiStrToLower(str);
   else
-    word = ICUToLowercase(str);
+    word = una::cases::to_lowercase_utf8(str);
 
   auto it = entries_.find(word);
   return (it != entries_.end()) ? &it->second : nullptr;
@@ -144,7 +99,7 @@ void BaseStringIndex::Remove(DocId id, DocumentAccessor* doc, string_view field)
 }
 
 absl::flat_hash_set<std::string> TextIndex::Tokenize(std::string_view value) const {
-  return ICUTokenizeWords(value);
+  return TokenizeWords(value);
 }
 
 absl::flat_hash_set<std::string> TagIndex::Tokenize(std::string_view value) const {
