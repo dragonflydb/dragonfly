c47469ed170e8aa66cc2ed688beb2062fda7d414
Yue Li <61070669+theyueli@users.noreply.github.com>
2023-09-27 00:14:57 -0700

    feat(streams): support XACK command (#1923)
    fixes #1869

--- src/server/stream_family.h
+++ src/server/stream_family.h
@@ -30,6 +30,7 @@ class StreamFamily {
   static void XSetId(CmdArgList args, ConnectionContext* cntx);
   static void XTrim(CmdArgList args, ConnectionContext* cntx);
   static void XRangeGeneric(CmdArgList args, bool is_rev, ConnectionContext* cntx);
+  static void XAck(CmdArgList args, ConnectionContext* cntx);
 };
 
 }  // namespace dfly
