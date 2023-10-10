e60202381882621ec47b0e1f48218cb300baad33
shahar <shahar@dragonflydb.io>
2023-10-10 13:33:14 +0300

    Fix

--- tests/dragonfly/instance.py
+++ tests/dragonfly/instance.py
@@ -75,8 +75,10 @@ class DflyInstance:
             self.args["alsologtostderr"] = None
 
         # Run with num_shards = (proactor_threads - 1) if possible, so help expose bugs
-        if "proactor_threads" in self.args and "num_shards" not in self.args:
-            threads = int(self.args["proactor_threads"])
+        if "num_shards" not in self.args:
+            threads = psutil.cpu_count()
+            if "proactor_threads" in self.args:
+                threads = int(self.args["proactor_threads"])
             if threads > 1:
                 self.args["num_shards"] = threads - 1
 
