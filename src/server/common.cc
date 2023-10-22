cdad0eedea8b7f479d63948bbd277ff24f4737dc
Roman Gershman <roman@dragonflydb.io>
2023-10-22 20:01:52 +0300

    chore: measure rss memory and track its peak
    Up until know we did not have cached rss metric in the process.
    This PR consolidates caching of all values together inside the EngineShard periodic fiber
    code. Also, we know expose rss_mem_current that can be used internally for identifying
    memory pressuring periods during the process run.
    
    Signed-off-by: Roman Gershman <roman@dragonflydb.io>

--- src/server/common.cc
+++ src/server/common.cc
@@ -64,6 +64,9 @@ string_view KeyLockArgs::GetLockKey(string_view key) {
 
 atomic_uint64_t used_mem_peak(0);
 atomic_uint64_t used_mem_current(0);
+atomic_uint64_t rss_mem_current(0);
+atomic_uint64_t rss_mem_peak(0);
+
 unsigned kernel_version = 0;
 size_t max_memory_limit = 0;
 
