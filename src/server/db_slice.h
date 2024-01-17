10938d6ccac8235ff642eee14a5ecc409be7ffe3
Yue Li <xyly781@gmail.com>
2024-01-17 12:41:16 -0800

    review comments

--- src/server/db_slice.h
+++ src/server/db_slice.h
@@ -471,19 +471,22 @@ class DbSlice {
 
   using HashSetAllocator = PMR_NS::polymorphic_allocator<facade::Connection::WeakRef>;
 
-  using AllocatorType = PMR_NS::polymorphic_allocator<std::pair<
-      std::string,
+  using ConnectionHashSet =
       absl::flat_hash_set<facade::Connection::WeakRef, Hash,
                           absl::container_internal::hash_default_eq<facade::Connection::WeakRef>,
-                          HashSetAllocator>>>;
+                          HashSetAllocator>;
 
-  absl::flat_hash_map<
-      std::string,
-      absl::flat_hash_set<facade::Connection::WeakRef, Hash,
-                          absl::container_internal::hash_default_eq<facade::Connection::WeakRef>,
-                          HashSetAllocator>,
-      absl::container_internal::hash_default_hash<std::string>,
-      absl::container_internal::hash_default_eq<std::string>, AllocatorType>
+  using AllocatorType = PMR_NS::polymorphic_allocator<std::pair<std::string, ConnectionHashSet>>;
+
+  // using AllocatorType = PMR_NS::polymorphic_allocator<std::pair<
+  //     std::string,
+  //     absl::flat_hash_set<facade::Connection::WeakRef, Hash,
+  //                         absl::container_internal::hash_default_eq<facade::Connection::WeakRef>,
+  //                         HashSetAllocator>>>;
+
+  absl::flat_hash_map<std::string, ConnectionHashSet,
+                      absl::container_internal::hash_default_hash<std::string>,
+                      absl::container_internal::hash_default_eq<std::string>, AllocatorType>
       client_tracking_map_;
 };
 
