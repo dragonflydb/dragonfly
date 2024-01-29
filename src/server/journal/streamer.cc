c299edc24f2eb67ff2fea069111d0717b6d246e2
Borys <borys@dragonflydb.io>
2024-01-29 13:32:17 +0200

    fix: add versiong check

--- src/server/journal/streamer.cc
+++ src/server/journal/streamer.cc
@@ -116,11 +116,10 @@ bool RestoreStreamer::ShouldWrite(SlotId slot_id) const {
 }
 
 void RestoreStreamer::WriteBucket(PrimeTable::bucket_iterator it) {
-  DCHECK_LE(it.GetVersion(), snapshot_version_);
-  it.SetVersion(snapshot_version_);
-
   bool is_data_present = false;
-  {
+
+  if (it.GetVersion() < snapshot_version_) {
+    it.SetVersion(snapshot_version_);
     FiberAtomicGuard fg;  // Can't switch fibers because that could invalidate iterator
     string key_buffer;    // we can reuse it
     for (; !it.is_done(); ++it) {
@@ -151,9 +150,7 @@ void RestoreStreamer::OnDbChange(DbIndex db_index, const DbSlice::ChangeReq& req
   PrimeTable* table = db_slice_->GetTables(0).first;
 
   if (const PrimeTable::bucket_iterator* bit = req.update()) {
-    if (bit->GetVersion() < snapshot_version_) {
-      WriteBucket(*bit);
-    }
+    WriteBucket(*bit);
   } else {
     string_view key = get<string_view>(req.change);
     table->CVCUponInsert(snapshot_version_, key, [this](PrimeTable::bucket_iterator it) {
