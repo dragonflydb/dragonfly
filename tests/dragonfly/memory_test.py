1e58095a0bf85ea805224a2a8bbf6a058ecb8ed9
shahar <shahar@dragonflydb.io>
2024-02-07 14:18:50 +0200

    Update test to use DEBUG POPULATE

--- tests/dragonfly/memory_test.py
+++ tests/dragonfly/memory_test.py
@@ -6,44 +6,29 @@ from .utility import *
 @pytest.mark.asyncio
 @pytest.mark.slow
 @pytest.mark.parametrize(
-    "type, val_size",
+    "type, keys, val_size, elements",
     [
-        (ValueType.JSON, 1_000),
-        (ValueType.SET, 1_000),
-        (ValueType.HSET, 1_000),
-        (ValueType.ZSET, 1_000),
-        (ValueType.LIST, 2_000),
-        (ValueType.STRING, 1_000),
+        ("JSON", 300_000, 100, 100),
+        # ("SET", 500_000, 100, 100),
+        ("HSET", 500_000, 100, 100),
+        # ("ZSET", 400_000, 100, 100),
+        # ("LIST", 500_000, 100, 100),
+        ("STRING", 10_000_000, 1000, 1),
     ],
 )
-async def test_acl_setuser(df_local_factory, df_seeder_factory, type, val_size):
+async def test_acl_setuser(async_client, type, keys, val_size, elements):
     # Create a Dragonfly and fill it up with `type` until it reaches `min_rss`, then make sure that
     # the gap between used_memory and rss is no more than `max_unaccounted`.
-    min_rss = 3 * 1024 * 1024 * 1024  # 2gb
+    min_rss = 5 * 1024 * 1024 * 1024  # 5gb
     max_unaccounted = 200 * 1024 * 1024  # 200mb
 
-    master = df_local_factory.create(proactor_threads=1)
-    df_local_factory.start_all([master])
-    c_master = master.client()
-
-    unsupported_types = [v for v in ValueType if not v == type]
-    seeder = df_seeder_factory.create(
-        port=master.port,
-        keys=10_000_000,  # Overkill because we stop when we reach min_rss
-        val_size=val_size,
-        batch_size=10_000,
-        unsupported_types=unsupported_types,
+    await async_client.execute_command(
+        f"DEBUG POPULATE {keys} {type} {val_size} RAND TYPE {type} ELEMENTS {elements}"
     )
-    seed_task = asyncio.create_task(seeder.run(target_deviation=0.1))
-    while True:
-        await asyncio.sleep(1)
-        rss = (await c_master.info("memory"))["used_memory_rss"]
-        if rss > min_rss:
-            seed_task.cancel()
-            break
 
-    info = await c_master.info("memory")
+    await asyncio.sleep(2)  # Wait for another RSS heartbeat update in Dragonfly
+
+    info = await async_client.info("memory")
     print(f'Used memory {info["used_memory"]}, rss {info["used_memory_rss"]}')
+    assert info["used_memory"] > min_rss, "Weak testcase: too little used memory"
     assert info["used_memory_rss"] - info["used_memory"] < max_unaccounted
-
-    await disconnect_clients(c_master)
