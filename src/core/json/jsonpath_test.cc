06d88ddc88381f60821e01fdf983c1779c54604f
Roman Gershman <roman@dragonflydb.io>
2024-02-09 15:41:22 +0200

    chore: parse a simple jsonpath (#2566)
    Also, add an ubuntu-20 reusable devcontainer.
    
    Signed-off-by: Roman Gershman <roman@dragonflydb.io>

--- src/core/json/jsonpath_test.cc
+++ src/core/json/jsonpath_test.cc
@@ -31,21 +31,28 @@ class JsonPathTest : public ::testing::Test {
 #define NEXT_TOK(tok_enum)                                    \
   {                                                           \
     auto tok = Lex();                                         \
-    ASSERT_EQ(tok.type_get(), Parser::token::TOK_##tok_enum); \
+    ASSERT_EQ(Parser::token::TOK_##tok_enum, tok.type_get()); \
   }
 
 #define NEXT_EQ(tok_enum, type, val)                          \
   {                                                           \
     auto tok = Lex();                                         \
-    ASSERT_EQ(tok.type_get(), Parser::token::TOK_##tok_enum); \
+    ASSERT_EQ(Parser::token::TOK_##tok_enum, tok.type_get()); \
     EXPECT_EQ(val, tok.value.as<type>());                     \
   }
 
 TEST_F(JsonPathTest, Scanner) {
-  SetInput("$.мага-зин2.book[0].title");
+  SetInput("$.мага-зин2.book[0].*");
   NEXT_TOK(ROOT);
   NEXT_TOK(DOT);
   NEXT_EQ(UNQ_STR, string, "мага-зин2");
+  NEXT_TOK(DOT);
+  NEXT_EQ(UNQ_STR, string, "book");
+  NEXT_TOK(LBRACKET);
+  NEXT_EQ(UINT, unsigned, 0);
+  NEXT_TOK(RBRACKET);
+  NEXT_TOK(DOT);
+  NEXT_TOK(WILDCARD);
 }
 
 }  // namespace dfly::json
