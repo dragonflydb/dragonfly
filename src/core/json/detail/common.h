df39d3a286245fc7bbeb0550ad028e419f060eef
Roman Gershman <roman@dragonflydb.io>
2024-02-27 11:50:24 +0200

    chore: move jsoncons path code into dedicated files (#2666)
    It's only a code move, without functional changes.
    This is in preparation to implementing the same path functionality
    for flexbuffers objects.
    
    Signed-off-by: Roman Gershman <roman@dragonflydb.io>

---
+++ src/core/json/detail/common.h
@@ -0,0 +1,13 @@
+// Copyright 2024, DragonflyDB authors.  All rights reserved.
+// See LICENSE for licensing terms.
+//
+
+#pragma once
+
+namespace dfly::json::detail {
+enum MatchStatus {
+  OUT_OF_BOUNDS,
+  MISMATCH,
+};
+
+}
