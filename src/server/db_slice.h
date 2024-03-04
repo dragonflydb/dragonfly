114400bd27b2183e22fded97dec0a06e74edc1bf
Yue Li <xyly781@gmail.com>
2024-03-04 15:28:38 -0800

    clean

--- src/server/db_slice.h
+++ src/server/db_slice.h
@@ -371,8 +371,6 @@ class DbSlice {
   void ScheduleForOffloadStep(DbIndex db_indx, size_t increase_goal_bytes);
   void DefragSecondTierStep(DbIndex db_indx);
 
-  void DefragSecondTierStep(DbIndex db_indx);
-
   int32_t GetNextSegmentForEviction(int32_t segment_id, DbIndex db_ind) const;
 
   const DbTableArray& databases() const {
