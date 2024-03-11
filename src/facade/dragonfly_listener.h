ecf9863733ad0161d66f089e541fd09d2e8d7484
Vladislav Oleshko <vlad@dragonflydb.io>
2024-03-11 10:40:07 +0300

    fix: fixes

--- src/facade/dragonfly_listener.h
+++ src/facade/dragonfly_listener.h
@@ -103,7 +103,7 @@ class DispatchTracker {
 
   std::vector<facade::Listener*> listeners_;
   facade::Connection* issuer_;
-  util::fb2::BlockingCounter bc_{0};
+  util::fb2::BlockingCounter bc_{0};  // tracks number of pending checkpoints
   bool ignore_paused_;
   bool ignore_blocked_;
 };
