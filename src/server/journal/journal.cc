cd58c2a27d1e457dfc0d3465f76f9497f9040efd
Roman Gershman <roman@dragonflydb.io>
2024-03-19 20:08:16 +0200

    chore: journal cleanup
    Signed-off-by: Roman Gershman <roman@dragonflydb.io>

--- src/server/journal/journal.cc
+++ src/server/journal/journal.cc
@@ -38,30 +38,12 @@ void Journal::StartInThread() {
   }
 }
 
-#if 0
-error_code Journal::OpenInThread(bool persistent, string_view dir) {
-
-
-  error_code ec;
-
-  if (persistent) {
-    ec = journal_slice.Open(dir);
-    if (ec) {
-      return ec;
-    }
-  }
-
-  return ec;
-}
-#endif
-
 error_code Journal::Close() {
-  CHECK(lameduck_.load(memory_order_relaxed));
-
   VLOG(1) << "Journal::Close";
 
-  fb2::Mutex ec_mu;
-  error_code res;
+  if (!journal_slice.IsOpen()) {
+    return {};
+  }
 
   lock_guard lk(state_mu_);
   auto close_cb = [&](auto*) {
@@ -70,18 +52,11 @@ error_code Journal::Close() {
     if (shard) {
       shard->set_journal(nullptr);
     }
-
-    /*auto ec = journal_slice.Close();
-
-    if (ec) {
-      lock_guard lk2(ec_mu);
-      res = ec;
-    }*/
   };
 
   shard_set->pool()->AwaitFiberOnAll(close_cb);
 
-  return res;
+  return {};
 }
 
 uint32_t Journal::RegisterOnChange(ChangeCallback cb) {
@@ -108,28 +83,10 @@ LSN Journal::GetLsn() const {
   return journal_slice.cur_lsn();
 }
 
-bool Journal::EnterLameDuck() {
-  if (!journal_slice.IsOpen()) {
-    return false;
-  }
-
-  bool val = false;
-  bool res = lameduck_.compare_exchange_strong(val, true, memory_order_acq_rel);
-  return res;
-}
-
 void Journal::RecordEntry(TxId txid, Op opcode, DbIndex dbid, unsigned shard_cnt,
                           std::optional<SlotId> slot, Entry::Payload payload, bool await) {
   journal_slice.AddLogRecord(Entry{txid, opcode, dbid, shard_cnt, slot, std::move(payload)}, await);
 }
-
-/*
-void Journal::OpArgs(TxId txid, Op opcode, Span keys) {
-  DCHECK(journal_slice.IsOpen());
-
-  journal_slice.AddLogRecord(txid, opcode);
-}
-*/
 
 }  // namespace journal
 }  // namespace dfly
