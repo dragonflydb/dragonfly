6e594807e0c5de48b96e850330d81616894a3a70
Roman Gershman <roman@dragonflydb.io>
2024-03-29 21:51:45 +0300

    chore: add SBF data structure
    Based on https://gsd.di.uminho.pt/members/cbm/ps/dbloom.pdf
    
    The data-structure itself is a growing list of bloom filters,
    where the next filter has exponentially larger capacity with exponentially tighter error bound.
    
    The Exist() goes over all the filters and it's enough that at least one of them returns a positive result.
    For Add(), we make ensure that all the existing filters do not have the element, as well as making sure that the last
    filter that is being filled does not cross its maximum designated capacity.
    
    Signed-off-by: Roman Gershman <roman@dragonflydb.io>

--- src/core/bloom.h
+++ src/core/bloom.h
@@ -6,8 +6,9 @@
 
 #include <cstdint>
 #include <string_view>
+#include <vector>
 
-typedef struct mi_heap_s mi_heap_t;
+#include "base/pmr/memory_resource.h"
 
 namespace dfly {
 
@@ -17,20 +18,33 @@ class Bloom {
   Bloom& operator=(const Bloom&) = delete;
 
  public:
-  /**
-   * @brief Construct a new Bloom object
-   *
-   * @param entries - entries are silently rounded up to the minimum capacity.
-   * @param error must be in (0, 1) range.
-   * @param heap
-   */
-  Bloom(uint32_t entries, double error, mi_heap_t* heap);
+  Bloom() = default;
+
+  /// @brief Destroy must be called before calling the d'tor
   ~Bloom();
 
-  Bloom(Bloom&&) = default;
-  Bloom& operator=(Bloom&&) = default;
+  /**
+   * @brief Initializes a new Bloom object
+   *
+   * @param entries - entries are silently rounded up to the minimum capacity.
+   * @param fp_prob - False-positive probability of collision. Must be in (0, 1) range.
+   * @param heap
+   */
+  void Init(uint64_t entries, double fp_prob, PMR_NS::memory_resource* resource);
+
+  /**
+   * @brief Destroys the object, must be called before destructing the object.
+   *
+   * @param resource - resource with which the object was initialized.
+   */
+  void Destroy(PMR_NS::memory_resource* resource);
+
+  Bloom(Bloom&& o);
 
   bool Exists(std::string_view str) const;
+
+  // Equivalent to the Exist above but accepts two fingerprints of the item.
+  bool Exists(uint64_t fp[2]) const;
 
   /**
    * @brief Adds an item to the bloom filter.
@@ -39,14 +53,48 @@ class Bloom {
    * @return false - if element (or a collision) had already been added previously.
    */
   bool Add(std::string_view str);
+  bool Add(uint64_t fp[2]);
+
+  size_t bitlen() const {
+    return 1ULL << bit_log_;
+  }
+
+  // Note that max element capacity is floor(bit_len / bpe), where bpe (bits per element) is
+  // derived from fp_prob.
+  size_t Capacity(double fp_prob) const;
 
  private:
   bool IsSet(size_t index) const;
   bool Set(size_t index);  // return true if bit was set (i.e was 0 before)
 
-  uint8_t hash_cnt_;
-  uint8_t bit_log_;  // log of bit length of the filter. bit length is always power of 2.
-  uint8_t* bf_;      // pointer to the blob.
+  uint8_t hash_cnt_ = 0;
+  uint8_t bit_log_ = 0;    // log of bit length of the filter. bit length is always power of 2.
+  uint8_t* bf_ = nullptr;  // pointer to the blob.
+};
+
+/**
+ * @brief Scalable bloom filter.
+ * Based on https://gsd.di.uminho.pt/members/cbm/ps/dbloom.pdf
+ * Please note that for SBF, the original paper assumes partitioning of bit space into K
+ * disjoint segments where K is number of hash functions. This is done to reduce index collisions.
+ * We do not do this, because we use power of 2 bit lengths.
+ * TODO: to test the actual rate of this filter.
+ */
+class SBF {
+ public:
+  SBF(uint64_t initial_capacity, double fp_prob, double grow_factor, PMR_NS::memory_resource* mr);
+  ~SBF();
+
+  bool Add(std::string_view str);
+  bool Exists(std::string_view str) const;
+
+ private:
+  // multiple filters from the smallest to the largest.
+  std::vector<Bloom, PMR_NS::polymorphic_allocator<Bloom>> filters_;
+  double grow_factor_;
+  double fp_prob_;
+  size_t current_size_ = 0;
+  size_t max_capacity_;
 };
 
 }  // namespace dfly
