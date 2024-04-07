934a8c64c90356ada0ed873529de24beb20f1524
Roman Gershman <roman@dragonflydb.io>
2024-04-07 10:49:00 +0300

    fix: healthcheck for docker containers (#2853)
    * fix: healthcheck for docker containers
    
    Fixes #2841
    
    ---------
    
    Signed-off-by: Roman Gershman <roman@dragonflydb.io>

--- tools/docker/healthcheck.sh
+++ tools/docker/healthcheck.sh
@@ -4,7 +4,9 @@ HOST="localhost"
 PORT=$HEALTHCHECK_PORT
 
 if [ -z "$HEALTHCHECK_PORT" ]; then
-    PORT=$(netstat -tuln | grep -oE ':[0-9]+' | grep -oE '[0-9]+' | tail -n 1)
+    # check all the TCP listening sockets, filter the dragonfly process, and fetch the port.
+    # For cases when dragonfly opens multiple ports, we filter with tail to choose one of them.
+    PORT=$(netstat -tlnp | grep "1/dragonfly" | grep -oE ':[0-9]+' | cut -c2- | tail -n 1)
 fi
 
 # If we're running with TLS enabled, utilise OpenSSL for the check
