72c53dda102c9f68a1690a6d6aff841967af13ed
sunneydev <sunneydev@icloud.com>
2024-04-08 01:16:15 +0400

    test: add tests for http api auth bug

--- tests/dragonfly/http_conf_test.py
+++ tests/dragonfly/http_conf_test.py
@@ -75,3 +75,32 @@ async def test_password_on_admin(df_factory):
         async with aiohttp.ClientSession(auth=aiohttp.BasicAuth("default", "XXX")) as session:
             resp = await session.get(f"http://localhost:{server.admin_port}/")
             assert resp.status == 200
+            
+async def test_no_password_on_http_api(df_factory):
+    with df_factory.create(
+        port=1112,
+    ) as server:
+        async with aiohttp.ClientSession(auth=aiohttp.BasicAuth("default", "XXX")) as session:
+            resp = await session.post(f"http://localhost:{server.port}/api", json={["ping"]})
+            assert resp.status == 200
+        async with aiohttp.ClientSession(auth=aiohttp.BasicAuth("random")) as session:
+            resp = await session.post(f"http://localhost:{server.port}/api", json={["ping"]})
+            assert resp.status == 200
+        async with aiohttp.ClientSession() as session:
+            resp = await session.post(f"http://localhost:{server.port}/api", json={["ping"]})
+            assert resp.status == 200
+
+async def test_password_on_http_api(df_factory):
+    with df_factory.create(
+        port=1112,
+        requirepass="XXX",
+    ) as server:
+        async with aiohttp.ClientSession(auth=aiohttp.BasicAuth("default", "badpass")) as session:
+            resp = await session.post(f"http://localhost:{server.port}/api", json={["ping"]})
+            assert resp.status == 401
+        async with aiohttp.ClientSession() as session:
+            resp = await session.post(f"http://localhost:{server.port}/api", json={["ping"]})
+            assert resp.status == 401
+        async with aiohttp.ClientSession(auth=aiohttp.BasicAuth("default", "XXX")) as session:
+            resp = await session.post(f"http://localhost:{server.port}/api", json={["ping"]})
+            assert resp.status == 200
