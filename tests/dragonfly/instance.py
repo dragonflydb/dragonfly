161c04824ee1ad0c8e2881817f538a5356a406ba
Roman Gershman <roman@dragonflydb.io>
2024-05-01 11:50:50 +0300

    chore: remove version checks when running our regtests

--- tests/dragonfly/instance.py
+++ tests/dragonfly/instance.py
@@ -331,6 +331,7 @@ class DflyInstanceFactory:
         args = {**self.args, **kwargs}
         args.setdefault("dbfilename", "")
         args.setdefault("enable_direct_fd", None)  # Testing iouring with direct_fd enabled.
+        args.setdefault("noversion_check", None)
         # MacOs does not set it automatically, so we need to set it manually
         args.setdefault("maxmemory", "8G")
         vmod = "dragonfly_connection=1,accept_server=1,listener_interface=1,main_service=1,rdb_save=1,replica=1,cluster_family=1"
