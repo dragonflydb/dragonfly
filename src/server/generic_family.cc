ca984110aab46861dca7ed9ef03c208ccf1981f5
Vladislav <vlad@dragonflydb.io>
2024-06-30 09:15:14 +0300

    fix(server): Fix SCAN deadlock (#3235)

--- src/server/generic_family.cc
+++ src/server/generic_family.cc
@@ -543,10 +543,16 @@ uint64_t ScanGeneric(uint64_t cursor, const ScanOpts& scan_opts, StringVec* keys
   DbContext db_cntx{.db_index = cntx->conn_state.db_index, .time_now_ms = GetCurrentTimeMs()};
 
   do {
-    ess->Await(sid, [&] {
+    auto cb = [&] {
       OpArgs op_args{EngineShard::tlocal(), 0, db_cntx};
       OpScan(op_args, scan_opts, &cursor, keys);
-    });
+    };
+
+    // Avoid deadlocking, if called from shard queue script
+    if (EngineShard::tlocal() && EngineShard::tlocal()->shard_id() == sid)
+      cb();
+    else
+      ess->Await(sid, cb);
 
     if (cursor == 0) {
       ++sid;
