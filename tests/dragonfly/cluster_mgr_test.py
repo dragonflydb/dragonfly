53f05ada5448b6a74226fdbd5847fdc7fa68f16d
shahar <shahar@dragonflydb.io>
2024-07-07 13:03:16 +0300

    chore: Disable cluster_mgr_test

--- tests/dragonfly/cluster_mgr_test.py
+++ tests/dragonfly/cluster_mgr_test.py
@@ -22,6 +22,7 @@ def run_cluster_mgr(args):
     return result.returncode == 0
 
 
+@pytest.mark.skip(reason="Flaky on github regression action")
 @dfly_args({"proactor_threads": 2, "cluster_mode": "yes"})
 async def test_cluster_mgr(df_factory):
     NODES = 3
