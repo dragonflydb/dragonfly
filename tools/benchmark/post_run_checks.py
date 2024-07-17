e3eb8518fd09259f09ba8d97b1bab3540e9701fd
adiholden <adi@dragonflydb.io>
2024-07-17 14:34:48 +0300

    feat(test): Improve benchmark workflow (#3330)
    Signed-off-by: adi_holden <adi@dragonflydb.io>

--- tools/benchmark/post_run_checks.py
+++ tools/benchmark/post_run_checks.py
@@ -7,6 +7,10 @@ def main():
     max_unaccounted = 200 * 1024 * 1024  # 200mb
 
     client = redis.Redis(decode_responses=True)
+    info = client.info("server")
+    # Check version upgrade finsihed from last released version to last weekly docker build
+    assert info["dragonfly_version"] == "df-HEAD-HASH-NOTFOUND"
+
     info = client.info("memory")
     print(f'Used memory {info["used_memory"]}, rss {info["used_memory_rss"]}')
     assert info["used_memory_rss"] - info["used_memory"] < max_unaccounted
@@ -26,7 +30,11 @@ def main():
         time.sleep(1)
         replication_state = client.info("replication")["slave0"]
 
-    assert replication_state["lag"] == 0, f"Lag is bad, expected 0, got {replication_state['lag']}"
+    if replication_state["lag"] != 0:
+        print(f"Lag is bad, expected 0, got {replication_state['lag']}")
+        info = client.info("all")
+        print(f"Info all output: {info}")
+        assert False
 
 
 if __name__ == "__main__":
