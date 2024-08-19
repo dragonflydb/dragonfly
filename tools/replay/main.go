b9ab5b8fe7a31832e7c161820341fc63257c1192
Vladislav Oleshko <vlad@dragonflydb.io>
2024-08-19 17:08:03 +0300

    chore: print and analyze command

--- tools/replay/main.go
+++ tools/replay/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"sort"
+	"strings"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -28,10 +30,7 @@ func RenderTable(area *pterm.AreaPrinter, files []string, workers []FileWorker) 
 	area.Update(content)
 }
 
-func main() {
-	flag.Parse()
-	files := flag.Args()
-
+func Run(files []string) {
 	timeOffset := time.Now().Add(500 * time.Millisecond).Sub(DetermineBaseTime(files))
 	fmt.Println("Offset -> ", timeOffset)
 
@@ -62,4 +61,118 @@ func main() {
 	}
 
 	RenderTable(area, files, workers) // to show last stats
+}
+
+func Print(files []string) {
+	type StreamTop struct {
+		record Record
+		ch     chan Record
+	}
+
+	// Start file reader goroutines
+	var wg sync.WaitGroup
+	wg.Add(len(files))
+
+	tops := make([]StreamTop, len(files))
+	for i, file := range files {
+		tops[i].ch = make(chan Record, 100)
+		go func(ch chan Record, file string) {
+			parseRecords(file, func(r Record) bool {
+				ch <- r
+				return true
+			})
+			close(ch)
+			wg.Done()
+		}(tops[i].ch, file)
+	}
+
+	// Pick record with minimum time from each channel
+	for {
+		minTime := ^uint64(0)
+		minIndex := -1
+		for i := range tops {
+			if tops[i].record.Time == 0 {
+				if r, ok := <-tops[i].ch; ok {
+					tops[i].record = r
+				}
+			}
+
+			if rt := tops[i].record.Time; rt > 0 && rt < minTime {
+				minTime = rt
+				minIndex = i
+			}
+		}
+
+		if minIndex == -1 {
+			break
+		}
+
+		fmt.Println(tops[minIndex].record.values...)
+		tops[minIndex].record = Record{}
+	}
+
+	wg.Wait()
+}
+
+func Analyze(files []string) {
+	total := 0
+	chained := 0
+	clients := 0
+	cmdCounts := make(map[string]uint)
+
+	// count stats
+	for _, file := range files {
+		fileClients := make(map[uint32]bool)
+
+		parseRecords(file, func(r Record) bool {
+			total += 1
+			if r.HasMore > 0 {
+				chained += 1
+			}
+
+			fileClients[r.Client] = true
+			cmdCounts[r.values[0].(string)] += 1
+
+			return true
+		})
+
+		clients += len(fileClients)
+	}
+
+	// sort commands by frequencies
+	type Freq struct {
+		cmd   string
+		count uint
+	}
+	var sortedCmds []Freq
+	for cmd, count := range cmdCounts {
+		sortedCmds = append(sortedCmds, Freq{cmd, count})
+	}
+	sort.Slice(sortedCmds, func(i, j int) bool {
+		return sortedCmds[i].count > sortedCmds[j].count
+	})
+
+	// Print all the info
+	fmt.Println("Total commands", total)
+	fmt.Println("Has more%", 100*float32(chained)/float32(total))
+	fmt.Println("Total clients", clients)
+
+	for _, freq := range sortedCmds {
+		fmt.Printf("%8d | %v \n", freq.count, freq.cmd)
+	}
+}
+
+func main() {
+	flag.Parse()
+	cmd := flag.Arg(0)
+	files := flag.Args()[1:]
+
+	switch strings.ToLower(cmd) {
+	case "run":
+		Run(files)
+	case "print":
+		Print(files)
+	case "analyze":
+		Analyze(files)
+	}
 }
