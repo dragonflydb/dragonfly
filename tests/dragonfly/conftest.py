80e1dbb304ceca46a135fb0e4696e6325702e775
Roman Gershman <roman@dragonflydb.io>
2024-08-27 11:35:47 +0300

    chore: allow running dragonfly pytests in repeat (#3577)

--- tests/dragonfly/conftest.py
+++ tests/dragonfly/conftest.py
@@ -276,6 +276,22 @@ def pytest_addoption(parser):
         default=None,
         help="Provide a port to the existing memcached process for the test",
     )
+    parser.addoption("--repeat", action="store", help="Number of times to repeat each test")
+
+
+def pytest_generate_tests(metafunc):
+    if metafunc.config.option.repeat is not None:
+        count = int(metafunc.config.option.repeat)
+
+        # We're going to duplicate these tests by parametrizing them,
+        # which requires that each test has a fixture to accept the parameter.
+        # We can add a new fixture like so:
+        metafunc.fixturenames.append("tmp_ct")
+
+        # Now we parametrize. This is what happens when we do e.g.,
+        # @pytest.mark.parametrize('tmp_ct', range(count))
+        # def test_foo(): pass
+        metafunc.parametrize("tmp_ct", range(count))
 
 
 @pytest.fixture(scope="session")
