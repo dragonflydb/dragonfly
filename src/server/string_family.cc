c14c517a929df2c80d7ac7b0b7b7c1526361bc4b
Borys <borys@dragonflydb.io>
2024-09-23 16:50:11 +0300

    fix: setrange with empty value doesn't modify the DB

--- src/server/string_family.cc
+++ src/server/string_family.cc
@@ -109,7 +109,7 @@ OpResult<TResult<size_t>> OpSetRange(const OpArgs& op_args, string_view key, siz
   VLOG(2) << "SetRange(" << key << ", " << start << ", " << range << ")";
   auto& db_slice = op_args.GetDbSlice();
 
-  if (start + range.size() == 0) {
+  if (range.empty()) {
     return OpStrLen(op_args, key);
   }
 
@@ -1306,13 +1306,11 @@ void StringFamily::GetRange(CmdArgList args, ConnectionContext* cntx) {
 }
 
 void StringFamily::SetRange(CmdArgList args, ConnectionContext* cntx) {
-  string_view key = ArgS(args, 0);
-  string_view offset = ArgS(args, 1);
-  string_view value = ArgS(args, 2);
-  int32_t start;
+  CmdArgParser parser(args);
+  auto [key, start, value] = parser.Next<string_view, int32_t, string_view>();
 
-  if (!absl::SimpleAtoi(offset, &start)) {
-    return cntx->SendError(kInvalidIntErr);
+  if (auto err = parser.Error(); err) {
+    return cntx->SendError(err->MakeReply());
   }
 
   if (start < 0) {
