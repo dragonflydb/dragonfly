d53bb80839358982dc435a1f0c490d9be3a6ea21
adi_holden <adi@dragonflydb.io>
2024-10-01 14:03:41 +0300

    test: more stabe test_bgsave_and_save
    Signed-off-by: adi_holden <adi@dragonflydb.io>

--- tests/dragonfly/snapshot_test.py
+++ tests/dragonfly/snapshot_test.py
@@ -456,7 +456,7 @@ async def test_infomemory_while_snapshoting(df_factory, format: str):
 
 @dfly_args({**BASIC_ARGS, "dbfilename": "test-bgsave"})
 async def test_bgsave_and_save(async_client: aioredis.Redis):
-    await async_client.execute_command("DEBUG POPULATE 20000")
+    await async_client.execute_command("DEBUG POPULATE 200000")
 
     await async_client.execute_command("BGSAVE")
     with pytest.raises(redis.exceptions.ResponseError):
