540d24bab8b36d03d8ebf9ba39bcef10c582c588
kostas <kostas@dragonflydb.io>
2024-11-27 17:07:22 +0200

    cluster tests again

--- tests/dragonfly/cluster_test.py
+++ tests/dragonfly/cluster_test.py
@@ -1828,7 +1828,9 @@ async def test_cluster_migration_cancel(df_factory: DflyInstanceFactory):
         assert str(i) == await nodes[1].client.get(f"{{key50}}:{i}")
 
 
-@dfly_args({"proactor_threads": 2, "cluster_mode": "yes"})
+@dfly_args(
+    {"proactor_threads": 2, "cluster_mode": "yes", "disable_serialization_max_chunk_size": 0}
+)
 @pytest.mark.asyncio
 async def test_cluster_migration_huge_container(df_factory: DflyInstanceFactory):
     instances = [
