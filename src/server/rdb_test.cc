554eb6b05829c7dccabc4d0307d40ef63bb50725
Borys <borys@dragonflydb.io>
2024-12-04 14:09:37 +0200

    test: try to reproduce the issue

--- src/server/rdb_test.cc
+++ src/server/rdb_test.cc
@@ -646,6 +646,15 @@ TEST_F(RdbTest, LoadHugeStream) {
   ASSERT_EQ(2000, CheckedInt({"xlen", "test:0"}));
 }
 
+TEST_F(RdbTest, SnapshotTooBig) {
+  // Run({"debug", "populate", "10000", "foo", "1000"});
+  //  usleep(5000);  // let the stats to sync
+  max_memory_limit = 100000;
+  used_mem_current = 1000000;
+  auto resp = Run({"debug", "reload"});
+  ASSERT_THAT(resp, ErrArg("Out of memory"));
+}
+
 TEST_F(RdbTest, LoadStream2) {
   auto ec = LoadRdb("RDB_TYPE_STREAM_LISTPACKS_2.rdb");
   ASSERT_FALSE(ec) << ec.message();
@@ -668,15 +677,6 @@ TEST_F(RdbTest, LoadStream3) {
                   "last-generated-id", "1732614679549-0", "max-deleted-entry-id", "0-0",
                   "entries-added", IntArg(0), "recorded-first-entry-id", "0-0", "groups", IntArg(1),
                   "first-entry", ArgType(RespExpr::ARRAY), "last-entry", ArgType(RespExpr::ARRAY)));
-}
-
-TEST_F(RdbTest, SnapshotTooBig) {
-  // Run({"debug", "populate", "10000", "foo", "1000"});
-  //  usleep(5000);  // let the stats to sync
-  max_memory_limit = 100000;
-  used_mem_current = 1000000;
-  auto resp = Run({"debug", "reload"});
-  ASSERT_THAT(resp, ErrArg("Out of memory"));
 }
 
 }  // namespace dfly
