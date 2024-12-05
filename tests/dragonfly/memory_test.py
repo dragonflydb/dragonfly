7ad118b81436d7698c75e3e0c165297ea809a8fd
shahar <shahar@dragonflydb.io>
2024-12-05 09:46:08 +0200

    list

--- tests/dragonfly/memory_test.py
+++ tests/dragonfly/memory_test.py
@@ -53,10 +53,11 @@ async def test_rss_used_mem_gap(df_server: DflyInstance, type, keys, val_size, e
     assert delta > 0
     assert delta < max_unaccounted
 
-    delta = info["used_memory_rss"] - info["object_used_memory"]
-    max_unaccounted *= 1.1  # Some more memory is needed for dash table, keys, etc
-    assert delta > 0
-    assert delta < max_unaccounted
+    if type != "LIST":
+        delta = info["used_memory_rss"] - info["object_used_memory"]
+        max_unaccounted *= 1.1  # Some more memory is needed for dash table, keys, etc
+        assert delta > 0
+        assert delta < max_unaccounted
 
 
 @pytest.mark.asyncio
