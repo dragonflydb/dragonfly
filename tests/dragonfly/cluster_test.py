6a7931985b14fb141b940ecb0d05d368b26a36c3
Borys <borys@dragonflydb.io>
2024-12-18 15:43:45 +0200

    fix: cluster tests stability (#4338)

--- tests/dragonfly/cluster_test.py
+++ tests/dragonfly/cluster_test.py
@@ -920,7 +920,7 @@ async def test_cluster_flush_slots_after_config_change(df_factory: DflyInstanceF
     assert await c_replica.execute_command("dbsize") == (100_000 - slot_0_size)
 
 
-@dfly_args({"proactor_threads": 4, "cluster_mode": "yes", "admin_port": 30001})
+@dfly_args({"proactor_threads": 4, "cluster_mode": "yes", "admin_port": next(next_port)})
 async def test_cluster_blocking_command(df_server):
     c_master = df_server.client()
     c_master_admin = df_server.admin_client()
@@ -1451,7 +1451,7 @@ async def test_network_disconnect_during_migration(df_factory):
     await StaticSeeder(key_target=100000).run(nodes[0].client)
     start_capture = await StaticSeeder.capture(nodes[0].client)
 
-    proxy = Proxy("127.0.0.1", 1111, "127.0.0.1", nodes[1].instance.admin_port)
+    proxy = Proxy("127.0.0.1", next(next_port), "127.0.0.1", nodes[1].instance.admin_port)
     await proxy.start()
     task = asyncio.create_task(proxy.serve())
 
@@ -2163,7 +2163,7 @@ async def test_replicate_disconnect_cluster(df_factory: DflyInstanceFactory, df_
 
     fill_task = asyncio.create_task(seeder.run())
 
-    proxy = Proxy("127.0.0.1", 1114, "127.0.0.1", cluster_nodes[0].port)
+    proxy = Proxy("127.0.0.1", next(next_port), "127.0.0.1", cluster_nodes[0].port)
     await proxy.start()
     proxy_task = asyncio.create_task(proxy.serve())
 
@@ -2318,7 +2318,7 @@ async def test_replicate_disconnect_redis_cluster(redis_cluster, df_factory, df_
 
     fill_task = asyncio.create_task(seeder.run())
 
-    proxy = Proxy("127.0.0.1", 1114, "127.0.0.1", redis_cluster_nodes[1].port)
+    proxy = Proxy("127.0.0.1", next(next_port), "127.0.0.1", redis_cluster_nodes[1].port)
     await proxy.start()
     proxy_task = asyncio.create_task(proxy.serve())
 
@@ -2374,6 +2374,7 @@ async def test_replicate_disconnect_redis_cluster(redis_cluster, df_factory, df_
     await c_replica.execute_command("REPLICAOF NO ONE")
     capture = await seeder.capture()
     assert await seeder.compare(capture, replica.port)
+    await proxy.close(proxy_task)
 
 
 @pytest.mark.skip("Takes more than 10 minutes")
