e4a9a0a3fde7f82884421cf36d1bba63a6019561
Stepan Bagritsevich <stefan@dragonflydb.io>
2025-01-20 17:05:06 +0100

    refactor: address comments
    Signed-off-by: Stepan Bagritsevich <stefan@dragonflydb.io>

--- src/core/json/json_object.cc
+++ src/core/json/json_object.cc
@@ -4,16 +4,10 @@
 
 #include "core/json/json_object.h"
 
-#include "base/flags.h"
 #include "base/logging.h"
 
 using namespace jsoncons;
 using namespace std;
-
-ABSL_FLAG(uint32_t, json_nesting_depth_limit, 128,
-          "The maximum allowed nesting depth for JSON documents. "
-          "Any attempt to create a document with a nesting depth greater than the specified limit "
-          "will result in an error.");
 
 namespace dfly {
 
@@ -24,11 +18,13 @@ optional<JsonType> JsonFromString(string_view input, PMR_NS::memory_resource* mr
     return false;
   };
 
-  /* The maximum possible JSON nesting depth is either the specified depth_limit or half of the
-     input size. Since nesting a JSON object requires at least 2 characters. */
-  const uint32_t depth_limit = absl::GetFlag(FLAGS_json_nesting_depth_limit);
-  auto parser_options =
-      jsoncons::json_options{}.max_nesting_depth(std::min(depth_limit, uint32_t(input.size() / 2)));
+  // The maximum allowed JSON nesting depth is 256.
+  const uint32_t json_nesting_depth_limit = 256;
+
+  /* The maximum possible JSON nesting depth is either the specified json_nesting_depth_limit or
+     half of the input size. Since nesting a JSON object requires at least 2 characters. */
+  auto parser_options = jsoncons::json_options{}.max_nesting_depth(
+      std::min(json_nesting_depth_limit, uint32_t(input.size() / 2)));
 
   json_decoder<JsonType> decoder(std::pmr::polymorphic_allocator<char>{mr});
   json_parser parser(parser_options, JsonErrorHandler);
