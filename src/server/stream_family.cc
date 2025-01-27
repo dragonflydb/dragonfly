b88748aa584e3f94ea1361f48db0362d8e4864ff
Borys <borys@dragonflydb.io>
2025-01-27 18:32:53 +0200

    fix: crash for delconsumer during stream reading (#4513)
    fix: crash for delconsumer during reading stream

--- src/server/stream_family.cc
+++ src/server/stream_family.cc
@@ -2337,10 +2337,11 @@ void XReadBlock(ReadOpts* opts, Transaction* tx, SinkReplyBuilder* builder,
       if (sitem.group) {
         range_opts.consumer =
             FindOrAddConsumer(opts->consumer_name, sitem.group, GetCurrentTimeMs());
-      }
+        sitem.consumer = range_opts.consumer;
+        if (!sitem.consumer) {
+          return OpStatus::OUT_OF_MEMORY;
+        }
 
-      range_opts.noack = opts->noack;
-      if (sitem.consumer) {
         if (sitem.consumer->pel->numnodes == 0) {
           LOG(DFATAL) << "Internal error when accessing consumer data, seen_time "
                       << sitem.consumer->seen_time;
@@ -2348,6 +2349,9 @@ void XReadBlock(ReadOpts* opts, Transaction* tx, SinkReplyBuilder* builder,
           return OpStatus::OK;
         }
       }
+
+      range_opts.noack = opts->noack;
+
       result = OpRange(t->GetOpArgs(shard), *wake_key, range_opts);
       key = *wake_key;
     }
