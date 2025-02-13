4a884bc6974bc9a40aa1947279a35d4d9c8910bc
Borys <borys@dragonflydb.io>
2025-02-13 13:22:26 +0200

    refactor: remove extra pipeline usage

--- tests/dragonfly/utility.py
+++ tests/dragonfly/utility.py
@@ -764,11 +764,9 @@ class ExpirySeeder:
     async def run(self, client):
         while not self.stop_flag:
             try:
-                pipeline = client.pipeline(transaction=True)
                 for i in range(0, self.batch_size):
-                    pipeline.execute_command(f"SET tmp{self.i} bar{self.i} EX {self.timeout}")
+                    await client.execute_command(f"SET tmp{self.i} bar{self.i} EX {self.timeout}")
                     self.i = self.i + 1
-                await pipeline.execute()
             except (redis.exceptions.ConnectionError, redis.exceptions.ResponseError) as e:
                 if self.stop_on_failure:
                     return
