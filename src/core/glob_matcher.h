bfcee6d9b07f74f79daf4fa7eb05bff1d017d947
Roman Gershman <roman@dragonflydb.io>
2025-02-19 12:57:17 +0200

    fix: revert reflex based matching due to performance problems
    Signed-off-by: Roman Gershman <roman@dragonflydb.io>

--- src/core/glob_matcher.h
+++ src/core/glob_matcher.h
@@ -23,12 +23,25 @@ class GlobMatcher {
   static std::string Glob2Regex(std::string_view glob);
 
  private:
+ // TODO: we fix the problem of stringmatchlen being much
+ // faster when the result is immediately known to be false, for example: "a*" vs "bxxxxx".
+ // The goal is to demonstrate on-par performance for the following case:
+ // > debug populate 5000000 keys 32 RAND
+ // > while true; do time valkey-cli scan 0 match 'foo*bar'; done
+ // Also demonstrate that the "improved" performance via SCAN command and not only via
+ // micro-benchmark.
+ // The performance of naive algorithm becomes worse in cases where string is long enough,
+ // and the pattern has a star at the start (or it matches at first).
+#ifdef FIX_PERFORMANCE_MATCHING
   mutable reflex::Matcher matcher_;
 
-  bool case_sensitive_;
   bool starts_with_star_ = false;
   bool ends_with_star_ = false;
   bool empty_pattern_ = false;
+#else
+  std::string glob_;
+#endif
+  bool case_sensitive_;
 };
 
 }  // namespace dfly
