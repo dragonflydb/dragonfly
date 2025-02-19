25c61459e9fbfa2def3f05bf5470f54a1fdc2e53
Roman Gershman <roman@dragonflydb.io>
2025-02-19 15:27:52 +0200

    fix: revert reflex based matching due to performance problems (#4626)
    Signed-off-by: Roman Gershman <roman@dragonflydb.io>

--- src/core/dfly_core_test.cc
+++ src/core/dfly_core_test.cc
@@ -52,123 +52,8 @@ static string GetRandomHex(size_t len) {
   return res;
 }
 
-/* Glob-style pattern matching taken from Redis. */
-static int stringmatchlen(const char* pattern, int patternLen, const char* string, int stringLen,
-                          int nocase) {
-  while (patternLen && stringLen) {
-    switch (pattern[0]) {
-      case '*':
-        while (patternLen && pattern[1] == '*') {
-          pattern++;
-          patternLen--;
-        }
-        if (patternLen == 1)
-          return 1; /* match */
-        while (stringLen) {
-          if (stringmatchlen(pattern + 1, patternLen - 1, string, stringLen, nocase))
-            return 1; /* match */
-          string++;
-          stringLen--;
-        }
-        return 0; /* no match */
-        break;
-      case '?':
-        string++;
-        stringLen--;
-        break;
-      case '[': {
-        int neg, match;
-
-        pattern++;
-        patternLen--;
-        neg = pattern[0] == '^';
-        if (neg) {
-          pattern++;
-          patternLen--;
-        }
-        match = 0;
-        while (1) {
-          if (pattern[0] == '\\' && patternLen >= 2) {
-            pattern++;
-            patternLen--;
-            if (pattern[0] == string[0])
-              match = 1;
-          } else if (pattern[0] == ']') {
-            break;
-          } else if (patternLen == 0) {
-            pattern--;
-            patternLen++;
-            break;
-          } else if (patternLen >= 3 && pattern[1] == '-') {
-            int start = pattern[0];
-            int end = pattern[2];
-            int c = string[0];
-            if (start > end) {
-              int t = start;
-              start = end;
-              end = t;
-            }
-            if (nocase) {
-              start = tolower(start);
-              end = tolower(end);
-              c = tolower(c);
-            }
-            pattern += 2;
-            patternLen -= 2;
-            if (c >= start && c <= end)
-              match = 1;
-          } else {
-            if (!nocase) {
-              if (pattern[0] == string[0])
-                match = 1;
-            } else {
-              if (tolower((int)pattern[0]) == tolower((int)string[0]))
-                match = 1;
-            }
-          }
-          pattern++;
-          patternLen--;
-        }
-        if (neg)
-          match = !match;
-        if (!match)
-          return 0; /* no match */
-        string++;
-        stringLen--;
-        break;
-      }
-      case '\\':
-        if (patternLen >= 2) {
-          pattern++;
-          patternLen--;
-        }
-        /* fall through */
-      default:
-        if (!nocase) {
-          if (pattern[0] != string[0])
-            return 0; /* no match */
-        } else {
-          if (tolower((int)pattern[0]) != tolower((int)string[0]))
-            return 0; /* no match */
-        }
-        string++;
-        stringLen--;
-        break;
-    }
-    pattern++;
-    patternLen--;
-    if (stringLen == 0) {
-      while (*pattern == '*') {
-        pattern++;
-        patternLen--;
-      }
-      break;
-    }
-  }
-  if (patternLen == 0 && stringLen == 0)
-    return 1;
-  return 0;
-}
+extern int stringmatchlen(const char* pattern, int patternLen, const char* string, int stringLen,
+                          int nocase);
 
 class TxQueueTest : public ::testing::Test {
  protected:
@@ -344,7 +229,16 @@ static void BM_MatchGlob(benchmark::State& state) {
     DoNotOptimize(matcher.Matches(random_val));
   }
 }
-BENCHMARK(BM_MatchGlob)->Arg(1000)->Arg(10000);
+BENCHMARK(BM_MatchGlob)->Arg(32)->Arg(1000)->Arg(10000);
+
+static void BM_MatchGlob2(benchmark::State& state) {
+  string random_val = GetRandomHex(state.range(0));
+  GlobMatcher matcher("bull:*:meta", true);
+  while (state.KeepRunning()) {
+    DoNotOptimize(matcher.Matches(random_val));
+  }
+}
+BENCHMARK(BM_MatchGlob2)->Arg(32)->Arg(1000)->Arg(10000);
 
 static void BM_MatchFindSubstr(benchmark::State& state) {
   string random_val = GetRandomHex(state.range(0));
@@ -395,6 +289,16 @@ static void BM_MatchRedisGlob(benchmark::State& state) {
   }
 }
 BENCHMARK(BM_MatchRedisGlob)->Arg(1000)->Arg(10000);
+
+static void BM_MatchRedisGlob2(benchmark::State& state) {
+  string random_val = GetRandomHex(state.range(0));
+  const char* pattern = "bull:*:meta";
+  while (state.KeepRunning()) {
+    DoNotOptimize(
+        stringmatchlen(pattern, strlen(pattern), random_val.c_str(), random_val.size(), 0));
+  }
+}
+BENCHMARK(BM_MatchRedisGlob2)->Arg(32)->Arg(1000)->Arg(10000);
 
 #ifdef USE_RE2
 static void BM_MatchRe2(benchmark::State& state) {
--- src/core/glob_matcher.cc
+++ src/core/glob_matcher.cc
@@ -11,6 +11,124 @@
 namespace dfly {
 using namespace std;
 
+/* Glob-style pattern matching taken from Redis. */
+int stringmatchlen(const char* pattern, int patternLen, const char* string, int stringLen,
+                   int nocase) {
+  while (patternLen && stringLen) {
+    switch (pattern[0]) {
+      case '*':
+        while (patternLen && pattern[1] == '*') {
+          pattern++;
+          patternLen--;
+        }
+        if (patternLen == 1)
+          return 1; /* match */
+        while (stringLen) {
+          if (stringmatchlen(pattern + 1, patternLen - 1, string, stringLen, nocase))
+            return 1; /* match */
+          string++;
+          stringLen--;
+        }
+        return 0; /* no match */
+        break;
+      case '?':
+        string++;
+        stringLen--;
+        break;
+      case '[': {
+        int neg, match;
+
+        pattern++;
+        patternLen--;
+        neg = pattern[0] == '^';
+        if (neg) {
+          pattern++;
+          patternLen--;
+        }
+        match = 0;
+        while (1) {
+          if (pattern[0] == '\\' && patternLen >= 2) {
+            pattern++;
+            patternLen--;
+            if (pattern[0] == string[0])
+              match = 1;
+          } else if (pattern[0] == ']') {
+            break;
+          } else if (patternLen == 0) {
+            pattern--;
+            patternLen++;
+            break;
+          } else if (patternLen >= 3 && pattern[1] == '-') {
+            int start = pattern[0];
+            int end = pattern[2];
+            int c = string[0];
+            if (start > end) {
+              int t = start;
+              start = end;
+              end = t;
+            }
+            if (nocase) {
+              start = tolower(start);
+              end = tolower(end);
+              c = tolower(c);
+            }
+            pattern += 2;
+            patternLen -= 2;
+            if (c >= start && c <= end)
+              match = 1;
+          } else {
+            if (!nocase) {
+              if (pattern[0] == string[0])
+                match = 1;
+            } else {
+              if (tolower((int)pattern[0]) == tolower((int)string[0]))
+                match = 1;
+            }
+          }
+          pattern++;
+          patternLen--;
+        }
+        if (neg)
+          match = !match;
+        if (!match)
+          return 0; /* no match */
+        string++;
+        stringLen--;
+        break;
+      }
+      case '\\':
+        if (patternLen >= 2) {
+          pattern++;
+          patternLen--;
+        }
+        /* fall through */
+      default:
+        if (!nocase) {
+          if (pattern[0] != string[0])
+            return 0; /* no match */
+        } else {
+          if (tolower((int)pattern[0]) != tolower((int)string[0]))
+            return 0; /* no match */
+        }
+        string++;
+        stringLen--;
+        break;
+    }
+    pattern++;
+    patternLen--;
+    if (stringLen == 0) {
+      while (*pattern == '*') {
+        pattern++;
+        patternLen--;
+      }
+      break;
+    }
+  }
+  if (patternLen == 0 && stringLen == 0)
+    return 1;
+  return 0;
+}
+
 string GlobMatcher::Glob2Regex(string_view glob) {
   string regex;
   regex.reserve(glob.size());
@@ -81,6 +199,7 @@ string GlobMatcher::Glob2Regex(string_view glob) {
 
 GlobMatcher::GlobMatcher(string_view pattern, bool case_sensitive)
     : case_sensitive_(case_sensitive) {
+#ifdef FIX_PERFORMANCE_MATCHING
   if (!pattern.empty()) {
     starts_with_star_ = pattern.front() == '*';
     pattern.remove_prefix(starts_with_star_);
@@ -99,9 +218,13 @@ GlobMatcher::GlobMatcher(string_view pattern, bool case_sensitive)
   regex.push_back(')');
   regex.append(Glob2Regex(pattern));
   matcher_.pattern(regex);
+#else
+  glob_ = pattern;
+#endif
 }
 
 bool GlobMatcher::Matches(std::string_view str) const {
+#ifdef FIX_PERFORMANCE_MATCHING
   DCHECK(!matcher_.pattern().empty());
 
   matcher_.input(reflex::Input(str.data(), str.size()));
@@ -128,6 +251,9 @@ bool GlobMatcher::Matches(std::string_view str) const {
   }
 
   return true;
+#else
+  return stringmatchlen(glob_.data(), glob_.size(), str.data(), str.size(), !case_sensitive_);
+#endif
 }
 
 }  // namespace dfly
