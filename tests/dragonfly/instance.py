11633bce4974427776308982a1a02cc75f58d92f
Volodymyr Yavdoshenko <volodymyr@dragonflydb.io>
2025-03-05 14:12:12 +0200

    fix: --skip_basic_usage flag was added to pytest execution

--- tests/dragonfly/instance.py
+++ tests/dragonfly/instance.py
@@ -420,6 +420,7 @@ class DflyInstanceFactory:
         vmod = "dragonfly_connection=1,accept_server=1,listener_interface=1,main_service=1,rdb_save=1,replica=1,cluster_family=1,proactor_pool=1,dflycmd=1,snapshot=1,streamer=1"
         args.setdefault("vmodule", vmod)
         args.setdefault("jsonpathv2")
+        args.setdefault("skip_basic_usage")
 
         # If path is not set, we assume that we are running the latest dragonfly.
         if not path:
