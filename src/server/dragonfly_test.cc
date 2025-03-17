cf015a7a8d131f103d71921adb55b75379babb06
Abhijat Malviya <abhijat@dragonflydb.io>
2025-03-17 13:44:17 +0530

    tests for aliasing
    Signed-off-by: Abhijat Malviya <abhijat@dragonflydb.io>

--- src/server/dragonfly_test.cc
+++ src/server/dragonfly_test.cc
@@ -25,6 +25,7 @@ ABSL_DECLARE_FLAG(float, mem_defrag_threshold);
 ABSL_DECLARE_FLAG(float, mem_defrag_waste_threshold);
 ABSL_DECLARE_FLAG(uint32_t, mem_defrag_check_sec_interval);
 ABSL_DECLARE_FLAG(std::vector<std::string>, rename_command);
+ABSL_DECLARE_FLAG(std::vector<std::string>, command_alias);
 ABSL_DECLARE_FLAG(bool, lua_resp2_legacy_float);
 ABSL_DECLARE_FLAG(double, eviction_memory_budget_threshold);
 
@@ -109,17 +110,13 @@ TEST_F(DflyEngineTest, Sds) {
 
 class DflyRenameCommandTest : public DflyEngineTest {
  protected:
-  DflyRenameCommandTest() : DflyEngineTest() {
+  DflyRenameCommandTest() {
     // rename flushall to myflushall, flushdb command will not be able to execute
     absl::SetFlag(
         &FLAGS_rename_command,
         std::vector<std::string>({"flushall=myflushall", "flushdb=", "ping=abcdefghijklmnop"}));
   }
-
-  void TearDown() {
-    absl::SetFlag(&FLAGS_rename_command, std::vector<std::string>({""}));
-    DflyEngineTest::TearDown();
-  }
+  absl::FlagSaver saver_;
 };
 
 TEST_F(DflyRenameCommandTest, RenameCommand) {
@@ -844,6 +841,30 @@ TEST_F(DflyEngineTest, CommandMetricLabels) {
   EXPECT_EQ(metrics.facade_stats.conn_stats.command_cnt_main, 0);
   EXPECT_EQ(metrics.facade_stats.conn_stats.num_conns_main, 0);
   EXPECT_EQ(metrics.facade_stats.conn_stats.num_conns_other, 0);
+}
+
+class DflyCommandAliasTest : public DflyEngineTest {
+ protected:
+  DflyCommandAliasTest() {
+    // Test an interaction of rename and alias, where we rename and then add an alias on the rename
+    absl::SetFlag(&FLAGS_rename_command, {"ping=gnip"});
+    absl::SetFlag(&FLAGS_command_alias, {"___set=set", "___ping=gnip"});
+  }
+
+  absl::FlagSaver saver_;
+};
+
+TEST_F(DflyCommandAliasTest, Aliasing) {
+  EXPECT_EQ(Run({"SET", "foo", "bar"}), "OK");
+  EXPECT_EQ(Run({"___SET", "a", "b"}), "OK");
+  EXPECT_EQ(Run({"GET", "foo"}), "bar");
+  EXPECT_EQ(Run({"GET", "a"}), "b");
+  // test the alias
+  EXPECT_EQ(Run({"___ping"}), "PONG");
+  // test the rename
+  EXPECT_EQ(Run({"gnip"}), "PONG");
+  // the original command is not accessible
+  EXPECT_THAT(Run({"PING"}), ErrArg("unknown command `PING`"));
 }
 
 }  // namespace dfly
