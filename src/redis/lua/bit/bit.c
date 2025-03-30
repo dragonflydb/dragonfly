188103de2935bc98bd17e2ad55fd533bf54fa228
Abhijat Malviya <abhijat@dragonflydb.io>
2025-03-30 18:52:59 +0530

    fix(lua): Fix integer overflow
    When evaluating bit_tohex in lua a user can pass the smallest value in
    signed int range, the code tries to flip the sign which causes an
    integer overflow.
    
    Special case handling is added to protect against INT32_MIN.
    
    Signed-off-by: Abhijat Malviya <abhijat@dragonflydb.io>

--- src/redis/lua/bit/bit.c
+++ src/redis/lua/bit/bit.c
@@ -135,10 +135,15 @@ static int bit_tohex(lua_State *L)
   SBits n = lua_isnone(L, 2) ? 8 : (SBits)barg(L, 2);
   const char *hexdigits = "0123456789abcdef";
   char buf[8];
-  int i;
-  if (n < 0) { n = -n; hexdigits = "0123456789ABCDEF"; }
+  if (n < 0) {
+    if (n == INT32_MIN) {
+      n += 1;
+    }
+    n = -n;
+    hexdigits = "0123456789ABCDEF";
+  }
   if (n > 8) n = 8;
-  for (i = (int)n; --i >= 0; ) { buf[i] = hexdigits[b & 15]; b >>= 4; }
+  for (int i = (int)n; --i >= 0; ) { buf[i] = hexdigits[b & 15]; b >>= 4; }
   lua_pushlstring(L, buf, (size_t)n);
   return 1;
 }
