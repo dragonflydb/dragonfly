f5a1b482aaee5a194da9117bf56bde682347ae70
adiholden <adi@dragonflydb.io>
2025-04-02 18:18:34 +0300

    feat(server): add connections send timeout (#4874)
    Signed-off-by: adi_holden <adi@dragonflydb.io>

--- src/server/main_service.cc
+++ src/server/main_service.cc
@@ -793,6 +793,7 @@ void Service::Init(util::AcceptServer* acceptor, std::vector<facade::Listener*> 
   config_registry.RegisterMutable("table_growth_margin");
   config_registry.RegisterMutable("tcp_keepalive");
   config_registry.RegisterMutable("timeout");
+  config_registry.RegisterMutable("send_timeout");
   config_registry.RegisterMutable("managed_service_info");
 
   config_registry.RegisterMutable(
