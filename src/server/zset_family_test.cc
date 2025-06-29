5464dfb00356f7b5466211414e294153ec8cd20f
Roman Gershman <roman@dragonflydb.io>
2025-06-29 09:08:24 +0300

    fix(zset): expand parsing of rank arguments beyond int32
    Fix clang-tidy warnings in zset_family code as well.
    
    Fixes #5374
    
    Signed-off-by: Roman Gershman <roman@dragonflydb.io>

--- src/server/zset_family_test.cc
+++ src/server/zset_family_test.cc
@@ -305,7 +305,7 @@ TEST_F(ZSetFamilyTest, ZMScoreNonExistentKeys) {
               ElementsAre(ArgType(RespExpr::NIL), ArgType(RespExpr::NIL), ArgType(RespExpr::NIL)));
 }
 
-TEST_F(ZSetFamilyTest, ZRangeRank) {
+TEST_F(ZSetFamilyTest, ByScore) {
   Run({"zadd", "x", "1.1", "a", "2.1", "b"});
   EXPECT_THAT(Run({"zrangebyscore", "x", "0", "(1.1"}), ArrLen(0));
   EXPECT_THAT(Run({"zrangebyscore", "x", "-inf", "1.1", "limit", "0", "10"}), "a");
@@ -506,6 +506,9 @@ TEST_F(ZSetFamilyTest, ZRange) {
   resp = Run({"zrange", "key", "+", "[cool", "BYLEX", "LIMIT", "2", "2", "REV"});
   ASSERT_THAT(resp, ArgType(RespExpr::ARRAY));
   EXPECT_THAT(resp.GetVec(), ElementsAre("great", "foo"));
+
+  resp = Run({"zrange", "key", "5", "2147483648"});
+  ASSERT_THAT(resp, RespElementsAre("foo", "great", "hill", "omega"));
 }
 
 TEST_F(ZSetFamilyTest, ZRevRange) {
