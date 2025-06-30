deae1fad1d9ac84283a5681dfeb41776e6f90257
Boomballa <boomballa@126.com>
2025-06-30 10:26:53 +0800

    feat(server): config info reporting and config_file rewrite support

--- src/server/server_family.cc
+++ src/server/server_family.cc
@@ -3574,9 +3574,11 @@ void ServerFamily::Register(CommandRegistry* registry) {
 
 }  // namespace dfly
 
+namespace dfly {
 std::string g_config_file_path;
 std::string g_executable_path;
 void SetInfoServerGlobals(const std::string& config_file_path, const std::string& executable_path) {
     g_config_file_path = config_file_path;
     g_executable_path = executable_path;
 }
+} // namespace dfly
