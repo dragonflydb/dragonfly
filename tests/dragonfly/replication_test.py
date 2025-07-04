56fbc6cc0655e8f43f951b9fdbfcecc56855a386
mkaruza <mario@dragonflydb.io>
2025-07-04 09:59:42 +0200

    fix(pytest): Increase REPLTAKOVER time in test_take_over_seeder
    REPLTAKEVER fails when we are replication from 4 proactor threads to 2.
    Increasing timeout should fix this test.
    
    Closes #5360
    
    Signed-off-by: mkaruza <mario@dragonflydb.io>

--- tests/dragonfly/replication_test.py
+++ tests/dragonfly/replication_test.py
@@ -1294,7 +1294,7 @@ async def test_take_over_seeder(
     # Give the seeder a bit of time.
     await asyncio.sleep(3)
     logging.debug("running repltakover")
-    await c_replica.execute_command(f"REPLTAKEOVER 10 SAVE")
+    await c_replica.execute_command(f"REPLTAKEOVER 30 SAVE")
     logging.debug("after running repltakover")
     seeder.stop()
     await fill_task
