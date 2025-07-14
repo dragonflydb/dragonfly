46bf526d2114f3dd5dca261234aaabfa37f7a98d
Abhijat Malviya <abhijat@dragonflydb.io>
2025-07-14 11:28:09 +0530

    core: Add summary to page scan
    Signed-off-by: Abhijat Malviya <abhijat@dragonflydb.io>

--- src/core/page_usage_stats.cc
+++ src/core/page_usage_stats.cc
@@ -5,6 +5,8 @@
 #include "core/page_usage_stats.h"
 
 extern "C" {
+#include <unistd.h>
+
 #include "redis/zmalloc.h"
 mi_page_usage_stats_t mi_heap_page_is_underutilized(mi_heap_t* heap, void* p, float ratio);
 }
@@ -29,12 +31,30 @@ std::vector<mi_page_usage_stats_t> PageUsage::Stats() {
   return std::move(page_stats_);
 }
 
-bool PageUsage::ConsumePageStats(mi_page_usage_stats_t stats) {
-  const bool should_reallocate = stats.should_realloc;
-  if (collect_stats_ == CollectPageStats::YES) {
-    page_stats_.push_back(stats);
+const PageScanStats& PageUsage::Summary() const {
+  return summary_;
+}
+
+bool PageUsage::ConsumePageStats(mi_page_usage_stats_t page_stat) {
+  summary_.pages_scanned++;
+
+  const bool should_realloc = page_stat.should_realloc;
+  // The page is not full, not head of page queue and not heap mismatched, but still not
+  // considered for reallocation
+  const bool will_not_realloc = !should_realloc && !page_stat.is_malloc_page &&
+                                !page_stat.heap_mismatch && !page_stat.is_full;
+  if (will_not_realloc) {
+    summary_.pages_not_considered_for_realloc++;
   }
-  return should_reallocate;
+
+  if (collect_stats_ == CollectPageStats::YES) {
+    page_stats_.push_back(page_stat);
+    summary_.page_sizes.insert(page_stat.block_size);
+    if (will_not_realloc) {
+      summary_.data_for_page_not_realloc[page_stat.block_size] = page_stat;
+    }
+  }
+  return should_realloc;
 }
 
 }  // namespace dfly
--- src/core/page_usage_stats.h
+++ src/core/page_usage_stats.h
@@ -3,13 +3,22 @@
 //
 
 #pragma once
+#include <absl/container/flat_hash_map.h>
+#include <absl/container/flat_hash_set.h>
 #include <mimalloc/types.h>
 
 #include <vector>
 
 namespace dfly {
 
-enum class CollectPageStats { YES, NO };
+enum class CollectPageStats : uint8_t { YES, NO };
+
+struct PageScanStats {
+  size_t pages_scanned{0};
+  size_t pages_not_considered_for_realloc{0};
+  absl::flat_hash_set<size_t> page_sizes;
+  absl::flat_hash_map<uintptr_t, mi_page_usage_stats_t> data_for_page_not_realloc;
+};
 
 class PageUsage {
  public:
@@ -21,12 +30,15 @@ class PageUsage {
 
   std::vector<mi_page_usage_stats_t> Stats();
 
+  const PageScanStats& Summary() const;
+
  private:
-  bool ConsumePageStats(mi_page_usage_stats_t stats);
+  bool ConsumePageStats(mi_page_usage_stats_t page_stat);
 
   CollectPageStats collect_stats_{CollectPageStats::NO};
   float ratio_;
   std::vector<mi_page_usage_stats_t> page_stats_;
+  PageScanStats summary_;
 };
 
 }  // namespace dfly
