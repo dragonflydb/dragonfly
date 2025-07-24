9e219f34a2fb8764b8e83f0d2b6deca542780dd4
Abhijat Malviya <abhijat@dragonflydb.io>
2025-07-24 16:46:59 +0530

    core: Collect stats while defragmenting
    Signed-off-by: Abhijat Malviya <abhijat@dragonflydb.io>

--- src/core/page_usage_stats.h
+++ src/core/page_usage_stats.h
@@ -3,13 +3,56 @@
 //
 
 #pragma once
+
+#include <absl/container/btree_map.h>
 #include <mimalloc/types.h>
 
-#include <vector>
+#include "core/bloom.h"
+#include "server/tx_base.h"
+
+struct hdr_histogram;
 
 namespace dfly {
 
 enum class CollectPageStats : uint8_t { YES, NO };
+
+struct FilterWithSize {
+  SBF sbf;
+  size_t size;
+  void Add(uintptr_t);
+};
+
+struct CollectedPageStats {
+  double threshold{0.0};
+  uint64_t pages_scanned{0};
+  uint64_t pages_marked_for_realloc{0};
+  uint64_t pages_full{0};
+  uint64_t pages_reserved_for_malloc{0};
+  uint64_t pages_with_heap_mismatch{0};
+  uint64_t pages_above_threshold{0};
+
+  using ShardUsageSummary = absl::btree_map<uint8_t, uint64_t>;
+  ShardUsageSummary page_usage_hist;
+  absl::btree_map<ShardId, ShardUsageSummary> shard_wide_summary;
+
+  void Merge(CollectedPageStats&& other, ShardId shard_id);
+};
+
+struct UniquePages {
+  FilterWithSize pages_scanned;
+  FilterWithSize pages_marked_for_realloc;
+  FilterWithSize pages_full;
+  FilterWithSize pages_reserved_for_malloc;
+  FilterWithSize pages_with_heap_mismatch;
+  FilterWithSize pages_above_threshold;
+  hdr_histogram* page_usage_hist;
+
+  explicit UniquePages();
+  ~UniquePages();
+
+  void AddStat(mi_page_usage_stats_t stat);
+  CollectedPageStats CollectedStats() const;
+};
 
 class PageUsage {
  public:
@@ -19,8 +62,8 @@ class PageUsage {
 
   bool IsPageForObjectUnderUtilized(mi_heap_t* heap, void* object);
 
-  const std::vector<mi_page_usage_stats_t>& Stats() const {
-    return stats_;
+  CollectedPageStats CollectedStats() const {
+    return unique_pages_.CollectedStats();
   }
 
  private:
@@ -29,7 +72,7 @@ class PageUsage {
   CollectPageStats collect_stats_{CollectPageStats::NO};
   float threshold_;
 
-  std::vector<mi_page_usage_stats_t> stats_;
+  UniquePages unique_pages_;
 };
 
 }  // namespace dfly
