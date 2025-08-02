1bab841f983ba9274dc6bbf26e4a4ae8a985bc69
guozhihao3 <guozhihao3@xiaomi.com>
2025-08-02 11:03:06 +0800

    format

--- src/server/main_service.cc
+++ src/server/main_service.cc
@@ -2685,14 +2685,14 @@ void Service::Command(CmdArgList args, const CommandContext& cmd_cntx) {
   if (subcmd == "HELP" && sufficient_args) {
     // Return help information for supported COMMAND subcommands
     const string_view help[] = {
-      "(no subcommand)",
-      "    Return details about all commands.",
-      "COUNT",
-      "    Return the total number of commands in this server.",
-      "DOCS [<command-name> ...]",
-      "    Return documentation details about multiple commands.",
-      "    If no command names are given, documentation details for all",
-      "    commands are returned.",
+        "(no subcommand)",
+        "    Return details about all commands.",
+        "COUNT",
+        "    Return the total number of commands in this server.",
+        "DOCS [<command-name> ...]",
+        "    Return documentation details about multiple commands.",
+        "    If no command names are given, documentation details for all",
+        "    commands are returned.",
     };
     return rb->SendSimpleStrArr(help);
   }
