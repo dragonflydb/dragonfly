e63ec95c0cfd88fd49cfbcdf4764f6bebd76d0cd
Borys <borys@dragonflydb.io>
2025-08-06 16:12:48 +0300

    fix: fix UB in geouradius

--- src/server/geo_family.cc
+++ src/server/geo_family.cc
@@ -824,16 +824,14 @@ void GeoFamily::GeoRadius(CmdArgList args, const CommandContext& cmd_cntx) {
       default:
         // If MapNext failed, it means an unknown option was provided or
         // an option requiring an argument was missing its argument.
-        // The parser has already recorded the error. We retrieve it and send it.
-        DCHECK(parser.Error().has_value());
-        LOG_EVERY_T(INFO, 1) << "GeoRadius parser error: " << parser.Error()->MakeReply().ToSv();
-        return builder->SendError(parser.Error()->MakeReply());
+        // The parser has already recorded the error.
+        DCHECK(parser.HasError());
+        break;
     }
   }
 
   if (!parser.Finalize()) {
     auto error = parser.Error();
-    LOG_EVERY_T(INFO, 1) << "GeoRadius parser error: " << error->MakeReply().ToSv();
 
     switch (error->type) {
       case Errors::INVALID_LONG_LAT: {
