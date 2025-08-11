8c404daa07560f64cdaeece62aef043049a8866a
Vladislav Oleshko <vlad@dragonflydb.io>
2025-08-11 09:55:43 +0300

    small fixes

--- src/server/engine_shard.cc
+++ src/server/engine_shard.cc
@@ -423,8 +423,9 @@ void EngineShard::StartPeriodicHeartbeatFiber(util::ProactorBase* pb) {
 
   std::chrono::milliseconds period_ms(*cycle_ms);
 
+  fb2::Fiber::Opts fb_opts{.priority = fb2::FiberPriority::BACKGROUND, .name = "heatbeat"};
   fiber_heartbeat_periodic_ =
-      MakeFiber([this, index = pb->GetPoolIndex(), period_ms, heartbeat]() mutable {
+      fb2::Fiber(fb_opts, [this, index = pb->GetPoolIndex(), period_ms, heartbeat]() mutable {
         ThisFiber::SetName(absl::StrCat("heartbeat_periodic", index));
         RunFPeriodically(heartbeat, period_ms, "heartbeat", &fiber_heartbeat_periodic_done_);
       });
