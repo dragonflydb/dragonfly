940b2bbe5d2b72bef959494145993b8bb04a7f35
Kostas Kyrimis <kostas@dragonflydb.io>
2025-08-13 13:59:37 +0200

    chore: move tiering flags (#5665)
    Signed-off-by: kostas <kostas@dragonflydb.io>

--- src/server/main_service.cc
+++ src/server/main_service.cc
@@ -941,18 +941,7 @@ void Service::Init(util::AcceptServer* acceptor, std::vector<facade::Listener*> 
   config_registry.RegisterMutable("send_timeout");
   config_registry.RegisterMutable("managed_service_info");
 
-  // TODO(vlad): Introduce templatable flag cache
-  auto update_tiered_storage = [](auto) {
-    shard_set->pool()->AwaitBrief([](unsigned, auto*) {
-      if (auto* es = EngineShard::tlocal(); es && es->tiered_storage()) {
-        es->tiered_storage()->UpdateFromFlags();
-      }
-    });
-  };
-  config_registry.RegisterSetter<bool>("tiered_experimental_cooling", update_tiered_storage);
-  config_registry.RegisterSetter<unsigned>("tiered_storage_write_depth", update_tiered_storage);
-  config_registry.RegisterSetter<float>("tiered_offload_threshold", update_tiered_storage);
-  config_registry.RegisterSetter<float>("tiered_upload_threshold", update_tiered_storage);
+  RegisterTieringFlags();
 
   config_registry.RegisterMutable(
       "notify_keyspace_events", [pool = &pp_](const absl::CommandLineFlag& flag) {
@@ -2913,6 +2902,23 @@ void Service::OnConnectionClose(facade::ConnectionContext* cntx) {
   server_family_.OnClose(server_cntx);
 
   conn_state.tracking_info_.SetClientTracking(false);
+}
+
+void Service::RegisterTieringFlags() {
+#ifdef WITH_TIERING
+  // TODO(vlad): Introduce templatable flag cache
+  auto update_tiered_storage = [](auto) {
+    shard_set->pool()->AwaitBrief([](unsigned, auto*) {
+      if (auto* es = EngineShard::tlocal(); es && es->tiered_storage()) {
+        es->tiered_storage()->UpdateFromFlags();
+      }
+    });
+  };
+  config_registry.RegisterSetter<bool>("tiered_experimental_cooling", update_tiered_storage);
+  config_registry.RegisterSetter<unsigned>("tiered_storage_write_depth", update_tiered_storage);
+  config_registry.RegisterSetter<float>("tiered_offload_threshold", update_tiered_storage);
+  config_registry.RegisterSetter<float>("tiered_upload_threshold", update_tiered_storage);
+#endif
 }
 
 Service::ContextInfo Service::GetContextInfo(facade::ConnectionContext* cntx) const {
--- src/server/tiered_storage.cc
+++ src/server/tiered_storage.cc
@@ -1,4 +1,4 @@
-// Copyright 2022, DragonflyDB authors.  All rights reserved.
+// Copyright 2025, DragonflyDB authors.  All rights reserved.
 // See LICENSE for licensing terms.
 //
 
@@ -28,10 +28,18 @@
 
 using namespace facade;
 
-ABSL_DECLARE_FLAG(bool, tiered_experimental_cooling);
-ABSL_DECLARE_FLAG(unsigned, tiered_storage_write_depth);
-ABSL_DECLARE_FLAG(float, tiered_offload_threshold);
-ABSL_DECLARE_FLAG(float, tiered_upload_threshold);
+ABSL_FLAG(bool, tiered_experimental_cooling, true,
+          "If true, uses intermediate cooling layer "
+          "when offloading values to storage");
+
+ABSL_FLAG(unsigned, tiered_storage_write_depth, 50,
+          "Maximum number of concurrent stash requests issued by background offload");
+
+ABSL_FLAG(float, tiered_offload_threshold, 0.5,
+          "Ratio of free memory (free/max memory) below which offloading starts");
+
+ABSL_FLAG(float, tiered_upload_threshold, 0.1,
+          "Ratio of free memory (free/max memory) below which uploading stops");
 
 namespace dfly {
 
