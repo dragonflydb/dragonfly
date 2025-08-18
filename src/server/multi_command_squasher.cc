b5a3ddeb8dd8fc71dfa7b75de288e587c119fe29
Roman Gershman <roman@dragonflydb.io>
2025-08-18 19:11:19 +0300

    chore: add more detailed timings for slow squash hops (#5679)
    We identify a callback that likely to contribute to the slowness of squashing and try to understand the reason.
    The logging is tunable based on `log_squash_info_threshold_usec` parameter.
    
    ---------
    
    Signed-off-by: Roman Gershman <roman@dragonflydb.io>

--- src/server/multi_command_squasher.cc
+++ src/server/multi_command_squasher.cc
@@ -27,6 +27,7 @@ using base::CycleClock;
 namespace {
 
 thread_local uint64_t max_busy_squash_cycles_cached = 1ULL << 32;
+thread_local uint32_t log_squash_threshold_cached = 1ULL << 31;
 
 void CheckConnStateClean(const ConnectionState& state) {
   DCHECK_EQ(state.exec_info.state, ConnectionState::ExecInfo::EXEC_INACTIVE);
@@ -245,6 +246,11 @@ bool MultiCommandSquasher::ExecuteSquashed(facade::RedisReplyBuilder* rb) {
   Transaction* tx = cntx_->transaction;
   ServerState::tlocal()->stats.squash_width_freq_arr[num_shards - 1]++;
   uint64_t start = CycleClock::Now();
+  atomic_uint64_t max_sched_cycles{0}, max_exec_cycles{0};
+  base::SpinLock lock;
+  uint64_t fiber_running_cycles{0}, proactor_running_cycles{0};
+  uint32_t max_sched_thread_id, max_sched_seq_num{0};
+  vector<string> past_fibers;
 
   // Atomic transactions (that have all keys locked) perform hops and run squashed commands via
   // stubs, non-atomic ones just run the commands in parallel.
@@ -258,19 +264,47 @@ bool MultiCommandSquasher::ExecuteSquashed(facade::RedisReplyBuilder* rb) {
     fb2::BlockingCounter bc(num_shards);
     DVLOG(1) << "Squashing " << num_shards << " " << tx->DebugId();
 
-    auto cb = [this, bc, rb]() mutable {
+    // Saves work in case logging is disable (i.e. log_squash_threshold_cached is high).
+    const uint64_t min_threshold_cycles = CycleClock::FromUsec(log_squash_threshold_cached / 5);
+    auto cb = [&, bc, rb]() mutable {
+      uint64_t sched_time = CycleClock::Now() - start;
+
+      // Update max_sched_cycles in lock-free fashion, to avoid contention
+      uint64_t current = max_sched_cycles.load(memory_order_relaxed);
+      while (sched_time > min_threshold_cycles && sched_time > current) {
+        if (max_sched_cycles.compare_exchange_weak(current, sched_time, memory_order_relaxed,
+                                                   memory_order_relaxed)) {
+          lock_guard<base::SpinLock> g(lock);
+
+          // If it is still the longest scheduling time
+          if (max_sched_cycles.load(memory_order_relaxed) == sched_time) {
+            // Store the stats from the callback with longest scheduling time.
+            fiber_running_cycles = ThisFiber::GetRunningTimeCycles();
+            proactor_running_cycles = ProactorBase::me()->GetCurrentBusyCycles();
+            max_sched_thread_id = ProactorBase::me()->GetPoolIndex();
+            max_sched_seq_num = fb2::GetFiberRunSeq();
+            past_fibers = fb2::GetPastFiberNames();
+          }
+          break;
+        }
+        // current is updated to the current value of max_sched_cycles, so the loop will retry
+        // with the new value if sched_time is still greater than it.
+      }
+
       if (ThisFiber::GetRunningTimeCycles() > max_busy_squash_cycles_cached) {
         ThisFiber::Yield();
         stats_.yields++;
       }
       this->SquashedHopCb(EngineShard::tlocal(), rb->GetRespVersion());
       bc->Dec();
+      uint64_t exec_time = CycleClock::Now() - start;
+      current = max_exec_cycles.load(memory_order_relaxed);
+      while (exec_time > current) {
+        if (max_exec_cycles.compare_exchange_weak(current, exec_time, memory_order_relaxed,
+                                                  memory_order_relaxed))
+          break;
+      }
     };
-    unsigned run_usec = CycleClock::ToUsec(ThisFiber::GetRunningTimeCycles());
-    if (run_usec > 5'000) {
-      LOG_EVERY_T(WARNING, 1) << "Fiber run " << run_usec << " usec, squashed " << cmds_.size()
-                              << " commands";
-    }
     for (unsigned i = 0; i < sharded_.size(); ++i) {
       if (!sharded_[i].dispatched.empty())
         shard_set->AddL2(i, cb);
@@ -297,13 +331,30 @@ bool MultiCommandSquasher::ExecuteSquashed(facade::RedisReplyBuilder* rb) {
     if (aborted)
       break;
   }
+
   uint64_t after_reply = CycleClock::Now();
   uint64_t total_usec = CycleClock::ToUsec(after_reply - start);
-
   stats_.hop_usec += total_usec;
   stats_.reply_usec += CycleClock::ToUsec(after_reply - after_hop);
   stats_.hops++;
   stats_.squashed_commands += order_.size();
+
+  if (total_usec > log_squash_threshold_cached) {
+    uint64_t max_sched_usec = CycleClock::ToUsec(max_sched_cycles.load());
+    uint64_t fiber_running_usec = CycleClock::ToUsec(fiber_running_cycles);
+    uint64_t proactor_running_usec = CycleClock::ToUsec(proactor_running_cycles);
+    uint64_t max_exec_usec = CycleClock::ToUsec(max_exec_cycles.load());
+
+    LOG_EVERY_T(INFO, 0.1)
+        << "Squashed " << order_.size() << " commands. "
+        << "Total/Fanout/MaxSchedTime/ThreadCbTime/ThreadId/FiberCbTime/FiberSeq/MaxExecTime: "
+        << total_usec << "/" << num_shards_ << "/" << max_sched_usec << "/" << proactor_running_usec
+        << "/" << max_sched_thread_id << "/" << fiber_running_usec << "/"
+        << "/" << max_sched_seq_num << "/" << max_exec_usec
+        << "\n past fibers: " << absl::StrJoin(past_fibers, ", ")
+        << "\ncoordinator thread running time: "
+        << CycleClock::ToUsec(ProactorBase::me()->GetCurrentBusyCycles());
+  }
 
   tl_facade_stats->reply_stats.squashing_current_reply_size.fetch_sub(total_reply_size,
                                                                       std::memory_order_release);
@@ -362,6 +413,10 @@ bool MultiCommandSquasher::IsAtomic() const {
 
 void MultiCommandSquasher::SetMaxBusySquashUsec(uint32_t usec) {
   max_busy_squash_cycles_cached = CycleClock::FromUsec(usec);
+}
+
+void MultiCommandSquasher::SetLogSquashThreshold(uint32_t usec) {
+  log_squash_threshold_cached = usec;
 }
 
 }  // namespace dfly
