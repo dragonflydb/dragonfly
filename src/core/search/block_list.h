71a0c2a9d84e192acdc5a7f4c0b450215e84da16
Stepan Bagritsevich <stefan@dragonflydb.io>
2025-08-29 22:18:41 +0200

    fix(block_list): Speed up Split method
    Signed-off-by: Stepan Bagritsevich <stefan@dragonflydb.io>

--- src/core/search/block_list.h
+++ src/core/search/block_list.h
@@ -63,6 +63,7 @@ template <typename Container /* underlying container */> class BlockList {
 
   // Insert element, returns true if inserted, false if already present.
   bool Insert(ElementType t);
+  bool PushBack(ElementType t);
 
   // Remove element, returns true if removed, false if not found.
   bool Remove(ElementType t);
@@ -136,6 +137,8 @@ template <typename Container /* underlying container */> class BlockList {
   void TryMerge(BlockIt block);  // If needed, merge with previous block
   void TrySplit(BlockIt block);  // If needed, split into two blocks
 
+  void ReserveBlocks(size_t n);
+
   friend SplitResult Split(BlockList<SortedVector<std::pair<DocId, double>>>&& block_list);
 
  private:
@@ -158,7 +161,15 @@ template <typename T> class SortedVector {
   void Merge(SortedVector<T>&& other);
   std::pair<SortedVector<T>, SortedVector<T>> Split() &&;
 
-  size_t Size() {
+  T& operator[](size_t idx) {
+    return entries_[idx];
+  }
+
+  const T& operator[](size_t idx) const {
+    return entries_[idx];
+  }
+
+  size_t Size() const {
     return entries_.size();
   }
 
