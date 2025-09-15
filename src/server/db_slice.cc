61ed4e89cff80cc664d509f3f7d5aad0be03dff8
qlimenoque <49155800+qlimenoque@users.noreply.github.com>
2025-09-15 14:55:57 +0300

    feat: prometheus metrics per database (#5770)
    * feat: prometheus metrics per database
    
    ---------
    Co-authored-by: Roman Gershman <roman@dragonflydb.io>

--- src/server/db_slice.cc
+++ src/server/db_slice.cc
@@ -580,6 +580,7 @@ auto DbSlice::FindInternal(const Context& cntx, string_view key, optional<unsign
 
   if (!IsValid(res.it)) {
     events_.misses += miss_weight;
+    db.stats.events.misses += miss_weight;
     return OpStatus::KEY_NOTFOUND;
   }
 
@@ -589,6 +590,7 @@ auto DbSlice::FindInternal(const Context& cntx, string_view key, optional<unsign
 
   if (req_obj_type.has_value() && res.it->second.ObjType() != req_obj_type.value()) {
     events_.misses += miss_weight;
+    db.stats.events.misses += miss_weight;
     return OpStatus::WRONG_TYPE;
   }
 
@@ -596,6 +598,7 @@ auto DbSlice::FindInternal(const Context& cntx, string_view key, optional<unsign
     res = ExpireIfNeeded(cntx, res.it);
     if (!IsValid(res.it)) {
       events_.misses += miss_weight;
+      db.stats.events.misses += miss_weight;
       return OpStatus::KEY_NOTFOUND;
     }
   }
@@ -612,6 +615,7 @@ auto DbSlice::FindInternal(const Context& cntx, string_view key, optional<unsign
       break;
     case UpdateStatsMode::kReadStats:
       events_.hits++;
+      db.stats.events.hits++;
       if (db.slots_stats) {
         db.slots_stats[KeySlot(key)].total_reads++;
       }
@@ -778,6 +782,7 @@ OpResult<DbSlice::ItAndUpdater> DbSlice::AddOrFindInternal(const Context& cntx, 
   events_.garbage_collected = db.prime.garbage_collected();
   events_.stash_unloaded = db.prime.stash_unloaded();
   events_.evicted_keys += evp.evicted();
+  db.stats.events.evicted_keys += evp.evicted();
   events_.garbage_checked += evp.checked();
   if (db.slots_stats) {
     SlotId sid = KeySlot(key);
@@ -1264,6 +1269,7 @@ DbSlice::PrimeItAndExp DbSlice::ExpireIfNeeded(const Context& cntx, PrimeIterato
       ExpIterator(expire_it, StringOrView::FromView(key)), db.get());
 
   ++events_.expired_keys;
+  db->stats.events.expired_keys++;
 
   return {PrimeIterator{}, ExpireIterator{}};
 }
@@ -1493,6 +1499,7 @@ finish:
   SendQueuedInvalidationMessagesAsync();
   auto time_finish = absl::GetCurrentTimeNanos();
   events_.evicted_keys += evicted_items;
+  db_arr_[db_ind]->stats.events.evicted_keys += evicted_items;
   DVLOG(2) << "Eviction time (us): " << (time_finish - time_start) / 1000;
   return pair<uint64_t, size_t>{evicted_items, evicted_bytes};
 }
@@ -1582,6 +1589,11 @@ void DbSlice::ResetUpdateEvents() {
 
 void DbSlice::ResetEvents() {
   events_ = {};
+  for (auto& db : db_arr_) {
+    if (db) {
+      db->stats.events = {};
+    }
+  }
 }
 
 void DbSlice::SetNotifyKeyspaceEvents(std::string_view notify_keyspace_events) {
