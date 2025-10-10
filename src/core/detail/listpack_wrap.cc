225e54e2aacadc761b278d461abb00f11a20f878
Vladislav Oleshko <vlad@dragonflydb.io>
2025-10-10 12:57:53 +0300

    chore(hset_family): Mutable listpack wrapper functions
    Signed-off-by: Vladislav Oleshko <vlad@dragonflydb.io>

--- src/core/detail/listpack_wrap.cc
+++ src/core/detail/listpack_wrap.cc
@@ -34,9 +34,67 @@ void ListpackWrap::Iterator::Read() {
   next_ptr_ = lpNext(lp_, next_ptr_);
 }
 
+ListpackWrap::~ListpackWrap() {
+  DCHECK(!dirty_);
+}
+
+uint8_t* ListpackWrap::GetPointer() {
+  dirty_ = false;
+  return lp_;
+}
+
 ListpackWrap::Iterator ListpackWrap::Find(std::string_view key) const {
   uint8_t* ptr = lpFind(lp_, lpFirst(lp_), (unsigned char*)key.data(), key.size(), 1);
   return Iterator{lp_, ptr, intbuf_};
+}
+
+bool ListpackWrap::Delete(std::string_view key) {
+  uint8_t* ptr = lpFind(lp_, lpFirst(lp_), (unsigned char*)key.data(), key.size(), 1);
+  if (ptr == nullptr)
+    return false;
+
+  lp_ = lpDeleteRangeWithEntry(lp_, &ptr, 2);
+  dirty_ = true;
+  return true;
+}
+
+bool ListpackWrap::Insert(std::string_view key, std::string_view value, bool skip_exists) {
+  uint8_t* vptr;
+  uint8_t* fptr = lpFirst(lp_);
+  uint8_t* fsrc = key.empty() ? lp_ : (uint8_t*)key.data();
+  // if we vsrc is NULL then lpReplace will delete the element, which is not what we want.
+  // therefore, for an empty val we set it to some other valid address so that lpReplace
+  // will do the right thing and encode empty string instead of deleting the element.
+  uint8_t* vsrc = value.empty() ? lp_ : (uint8_t*)value.data();
+
+  bool updated = false;
+  if (fptr) {
+    fptr = lpFind(lp_, fptr, fsrc, key.size(), 1);
+    if (fptr) {
+      if (skip_exists)
+        return false;
+
+      // Grab pointer to the value (fptr points to the field)
+      vptr = lpNext(lp_, fptr);
+
+      // Replace value
+      lp_ = lpReplace(lp_, &vptr, vsrc, value.size());
+      DCHECK_EQ(0u, lpLength(lp_) % 2);
+
+      dirty_ = true;
+      updated = true;
+    }
+  }
+
+  if (!updated) {
+    // Push new field/value pair onto the tail of the listpack.
+    // TODO: we should at least allocate once for both elements
+    lp_ = lpAppend(lp_, fsrc, key.size());
+    lp_ = lpAppend(lp_, vsrc, value.size());
+    dirty_ = true;
+  }
+
+  return !updated;
 }
 
 size_t ListpackWrap::size() const {
