0386de03c4f627d989c26cedc0f024f91121c5f5
Abhijat Malviya <abhijat@dragonflydb.io>
2025-11-24 19:07:42 +0530

    tools: Add scripts and supporting files to generate os packge repo (#5923)
    * tools: Add scripts and supporting files to generate os packge repo
    * tests: Add automated tests to install packages for fedora,ubuntu
    
    Signed-off-by: Abhijat Malviya <abhijat@dragonflydb.io>

---
+++ tests/dragonfly/package_install_test.py
@@ -0,0 +1,33 @@
+from testcontainers.core.container import DockerContainer
+
+
+def check(container: DockerContainer, cmd: str):
+    result = container.exec(cmd)
+    assert result.exit_code == 0, f"command {cmd} failed with result {result}"
+
+
+async def test_install_package_on_fedora():
+    with DockerContainer(image="fedora:latest", tty=True) as fedora:
+        check(
+            fedora,
+            "dnf config-manager addrepo --from-repofile=https://packages.dragonflydb.io/dragonfly.repo",
+        )
+        check(fedora, "dnf -y install dragonfly")
+        check(fedora, "dragonfly --version")
+
+
+async def test_install_package_on_ubuntu():
+    with DockerContainer(image="ubuntu:latest", tty=True) as ubuntu:
+        check(ubuntu, "apt update")
+        check(ubuntu, "apt install -y curl")
+        check(
+            ubuntu,
+            "curl -Lo /usr/share/keyrings/dragonfly-keyring.public https://packages.dragonflydb.io/pgp-key.public",
+        )
+        check(
+            ubuntu,
+            "curl -Lo /etc/apt/sources.list.d/dragonfly.sources https://packages.dragonflydb.io/dragonfly.sources",
+        )
+        check(ubuntu, "apt update")
+        check(ubuntu, "apt install -y dragonfly")
+        check(ubuntu, "dragonfly --version")
