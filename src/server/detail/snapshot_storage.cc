dragonflydb/dragonfly

commit 7f39b9a09e3968537c3baf5207cf56608cbe327c

fix: loading of a snapshot from a cloud folder (#6280)
* fix: loading of a snapshot from a cloud folder

1. Fix mismatching a snapshot name when a folder has special characters (for example '+')
2. Fix wrong listing of objects under a (prefix) folder due to missing delimiter '/' in AwsS3SnapshotStorage::ExpandFromPath.
3. Fix adding the redundant `&prefix=` parameter when listing root paths.
4. Pulls helio with a fix of incorrect URI https://github.com/romange/helio/pull/520/
   Also update the aws-sdk version which should fix #5172

Fixes #6279 and #5172

Signed-off-by: Roman Gershman <roman@dragonflydb.io>

* chore: comments

---------

Signed-off-by: Roman Gershman <roman@dragonflydb.io>

src/server/detail/snapshot_storage.cc
@@ -61,6 +61,25 @@ pair<string, string> GetBucketPath(string_view path) {
 const int kRdbWriteFlags = O_CREAT | O_WRONLY | O_TRUNC | O_CLOEXEC | O_DIRECT;
 #endif
 
+std::string EscapeRegex(string_view input) {
+  // List of regex special characters that need escaping
+  constexpr std::string_view chars{"\\.^$|?*+()[]{}"};
+  std::string escaped;
+
+  // Reserve space to avoid multiple reallocations
+  escaped.reserve(input.size() * 1.1);
+
+  for (char c : input) {
+    // If the character is in our specialChars list, prepend a backslash
+    if (chars.find(c) != std::string::npos) {
+      escaped += '\\';
+    }
+    escaped += c;
+  }
+
+  return escaped;
+}
+
 }  // namespace
 
 string SnapshotStorage::FindMatchingFile(string_view prefix, string_view dbfilename,
@@ -72,6 +91,8 @@ string SnapshotStorage::FindMatchingFile(string_view prefix, string_view dbfilen
   // and adding an extension if needed.
   fs::path fl_path{prefix};
   fl_path.append(dbfilename);
+  fl_path = EscapeRegex(fl_path.string());
+
   SubstituteFilenamePlaceholders(&fl_path,
                                  {.ts = "([0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2})",
                                   .year = "([0-9]{4})",
@@ -83,6 +104,7 @@ string SnapshotStorage::FindMatchingFile(string_view prefix, string_view dbfilen
   const std::regex re(fl_path.string());
 
   for (const SnapStat& key : keys) {
+    DVLOG(1) << "Checking object key: " << key.name << " against regex: " << fl_path.string();
     std::smatch m;
     if (std::regex_match(key.name, m, re)) {
       return key.name;
@@ -586,12 +608,12 @@ io::Result<vector<string>, GenericError> AwsS3SnapshotStorage::ExpandFromPath(
     return nonstd::make_unexpected(
         GenericError{std::make_error_code(std::errc::invalid_argument), "Invalid S3 path"});
   }
-  const auto [bucket_name, obj_path] = *bucket_path;
-  const std::regex re(absl::StrReplaceAll(obj_path, {{"summary", "[0-9]{4}"}}));
+
+  auto& [bucket_name, obj_path] = *bucket_path;
 
   // Limit prefix to objects in the same 'directory' as load_path.
   const size_t pos = obj_path.find_last_of('/');
-  const std::string prefix = (pos == std::string_view::npos) ? "" : obj_path.substr(0, pos);
+  const std::string prefix = (pos == std::string_view::npos) ? "" : obj_path.substr(0, pos + 1);
 
   io::Result<std::vector<SnapStat>, GenericError> list_res = ListObjects(bucket_name, prefix);
   if (!list_res) {
@@ -599,8 +621,13 @@ io::Result<vector<string>, GenericError> AwsS3SnapshotStorage::ExpandFromPath(
   }
 
   vector<string> paths;
+  obj_path = EscapeRegex(obj_path);
+  const std::regex re(absl::StrReplaceAll(obj_path, {{"summary", "[0-9]{4}"}}));
+
   for (const SnapStat& key : *list_res) {
     std::smatch m;
+    DVLOG(1) << "Checking object key: " << key.name << " against regex: " << obj_path;
+
     if (std::regex_match(key.name, m, re)) {
       paths.push_back(std::string(kS3Prefix) + bucket_name + "/" + key.name);
     }
@@ -632,7 +659,10 @@ AwsS3SnapshotStorage::ListObjects(std::string_view bucket_name, std::string_view
   do {
     Aws::S3::Model::ListObjectsV2Request request;
     request.SetBucket(std::string(bucket_name));
-    request.SetPrefix(std::string(prefix));
+    if (!prefix.empty())
+      request.SetPrefix(std::string(prefix));
+    request.SetDelimiter("/");
+
     if (!continuation_token.empty()) {
       request.SetContinuationToken(continuation_token);
     }

commit 7309416b6d83b335aff1a0591ef6f34bef466ac9

chore: fix build error
1. Fix compile error in AWS specific code that was introduced by #6280
   Also configure properly S3ClientConfiguration in snapshot_storage, that is now required
   to preserve the existing behavior as we updated the AWS SDK client to the latest version.

2. Also bring a check that was mistakenly deleted by #5791

Signed-off-by: Roman Gershman <romange@gmail.com>

src/server/detail/snapshot_storage.cc
@@ -528,11 +528,14 @@ AwsS3SnapshotStorage::AwsS3SnapshotStorage(const std::string& endpoint, bool htt
     }
     // S3ClientConfiguration may request configuration and credentials from
     // EC2 metadata so must be run in a proactor thread.
-    Aws::S3::S3ClientConfiguration s3_conf{};
+    Aws::S3::S3ClientConfiguration s3_conf;
+    s3_conf.checksumConfig.responseChecksumValidation =
+        Aws::Client::ResponseChecksumValidation::WHEN_REQUIRED;
+
     LOG(INFO) << "Creating AWS S3 client; region=" << s3_conf.region << "; https=" << std::boolalpha
               << https << "; endpoint=" << endpoint;
     if (!sign_payload) {
-      s3_conf.payloadSigningPolicy = Aws::Client::AWSAuthV4Signer::PayloadSigningPolicy::ForceNever;
+      s3_conf.payloadSigningPolicy = Aws::Client::AWSAuthV4Signer::PayloadSigningPolicy::Never;
     }
     std::shared_ptr<Aws::Auth::AWSCredentialsProvider> credentials_provider =
         std::make_shared<aws::CredentialsProviderChain>();

src/server/engine_shard.cc
@@ -310,6 +310,11 @@ bool EngineShard::DefragTaskState::CheckRequired() {
     // finished checking.
     last_check_time = time(nullptr);
 
+    if (finfo.committed != finfo.committed_golden) {
+      LOG_FIRST_N(ERROR, 100) << "committed memory computed incorrectly: " << finfo.committed
+                              << " vs " << finfo.committed_golden;
+    }
+
     const double waste_threshold = GetFlag(FLAGS_mem_defrag_waste_threshold);
     if (finfo.wasted > size_t(finfo.committed * waste_threshold)) {
       VLOG(1) << "memory fragmentation issue found: " << finfo.wasted << " " << finfo.committed;