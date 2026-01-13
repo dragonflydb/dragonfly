fdcae74b044ff53462160aaa2786fc6f59fe202c
Kostas Kyrimis <kostas@dragonflydb.io>
2026-01-13 09:37:28 +0200

    format

--- src/server/conn_context.h
+++ src/server/conn_context.h
@@ -336,11 +336,11 @@ class ConnectionContext : public facade::ConnectionContext {
   // Username
   std::string authed_username{"default"};
 
-// Each entry in the list is a bitfield representing a specific command family, 
-// where each bit corresponds to an individual command within that family. 
-// Together, these entries encode the user's full ACL to commands. 
-// The index 'i' in 'acl_commands[i]' refers to the command family based on 
-// its registration order at runtime. For more details, see acl_commands_def.h.
+  // Each entry in the list is a bitfield representing a specific command family,
+  // where each bit corresponds to an individual command within that family.
+  // Together, these entries encode the user's full ACL to commands.
+  // The index 'i' in 'acl_commands[i]' refers to the command family based on
+  // its registration order at runtime. For more details, see acl_commands_def.h.
   std::vector<uint64_t> acl_commands;
 
   // Keyspace. Each key referenced in a command must match (any) of the rules (globs).
@@ -350,10 +350,10 @@ class ConnectionContext : public facade::ConnectionContext {
   dfly::acl::AclPubSub pub_sub;
 
   // db index, std::numeric_limits<size_t>::max for ALL db's. Dragonfly specific extension.
-  size_t acl_db_idx;
+  size_t acl_db_idx = std::numeric_limits<size_t>::max();
 
   // Skip ACL validation, used by internal commands and commands run on admin port
-  bool skip_acl_validation;
+  bool skip_acl_validation = false;
 
  private:
   void EnableMonitoring(bool enable) {
