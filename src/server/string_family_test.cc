9d5ed39a5a3069d215e9edc78f91bf7bbc318f5d
Vladislav Oleshko <vlad@dragonflydb.io>
2026-01-23 12:09:52 +0300

    chore(server): Simplify build file

--- src/server/string_family_test.cc
+++ src/server/string_family_test.cc
@@ -4,7 +4,6 @@
 #include "base/gtest.h"
 #include "base/logging.h"
 #include "facade/facade_test.h"
-#include "server/command_registry.h"
 #include "server/conn_context.h"
 #include "server/engine_shard_set.h"
 #include "server/error.h"
