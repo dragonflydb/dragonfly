ab7fcba7f0cbfb93d1ca7ffd51ac5cf78ad282e9
mkaruza <mario@dragonflydb.io>
2026-02-09 16:28:26 +0100

    fix(hnsw): Avoid possible HSET object memory misalignment
    Vector operations expect 4-byte memory alignment. If an HSET object is
    using listpack encoding, this cannot be guaranteed. Make pessimistic
    assumptions and force a copy of the vector data if it is possible that
    the HSET is using listpack.
    
    Closes #6546
    
    Signed-off-by: mkaruza <mario@dragonflydb.io>

--- src/server/search/global_hnsw_index.cc
+++ src/server/search/global_hnsw_index.cc
@@ -40,7 +40,16 @@ bool GlobalHnswIndexRegistry::Create(std::string_view index_name, std::string_vi
   if (it != indices_.end())
     return false;
 
-  const bool copy_vector = (data_type != DocIndex::HASH);
+  // We make a copy of vector data when:
+  // 1. Data type is JSON. This is because JSON object is not represented as contiguous memory.
+  // 2. Data type is HASH and vector data memory size is smaller than treshold for ListPack
+  // encoding.
+  //    We use pesimistic approach for decision and expect that ONLY VECTOR data field is used.
+  //    When HSET object is created function `IsGoodForListpack` decides if object should be encoded
+  //    as ListPack or StringMap. Problem with ListPack encoding is that vector memory, if
+  //    referenced, can have wrong alignment for vector distance operations.
+  const bool copy_vector =
+      (data_type == DocIndex::JSON) || (params.dim * 4 < server.max_listpack_map_bytes);
 
   indices_[key] = std::make_shared<search::HnswVectorIndex>(params, copy_vector);
 
