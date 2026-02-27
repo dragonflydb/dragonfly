d957f582c42ac7bca73c41ca9fc127859ae941ac
Abhijat Malviya <abhijat@dragonflydb.io>
2026-02-27 14:19:37 +0530

    test removing change
    Signed-off-by: Abhijat Malviya <abhijat@dragonflydb.io>

--- src/server/main_service.cc
+++ src/server/main_service.cc
@@ -1514,7 +1514,7 @@ DispatchResult Service::DispatchCommand(facade::ParsedArgs args, facade::ParsedC
 
   if (dfly_cntx->async_dispatch && cid->IsBlocking()) {
     ++ServerState::tlocal()->stats.blocking_commands_in_pipelines;
-    cmd_cntx->conn()->FlushReplies();
+    // cmd_cntx->conn()->FlushReplies();
   }
 
   ArgSlice tail_args;
