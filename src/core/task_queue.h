1b694a39c955c0ccc3c284b2a391ca1ea7774e39
Roman Gershman <roman@dragonflydb.io>
2026-06-14 12:20:03 +0300

    core: use fetch-and-add blocking enqueue for TaskQueue
    Bump helio to the fetch-and-add FiberQueue::Add and simplify dfly::TaskQueue: Add/Await now
    delegate straight to FiberQueue (no TryAdd-first fast path), the blocked_submitters_ gauge moves
    into FiberQueue, and the unused TaskQueue::TryAdd is removed.
    
    Signed-off-by: Roman Gershman <roman@dragonflydb.io>

--- src/core/task_queue.h
+++ src/core/task_queue.h
@@ -11,41 +11,20 @@ namespace dfly {
 
 /**
  *  MPSC task-queue that is handled by a single consumer thread.
- *  The queue is just a wrapper around FiberQueue that manages its fiber itself.
+ *  The queue is just a wrapper around FiberQueue that manages its fibers itself.
  */
 class TaskQueue {
  public:
   // TODO: to add a mechanism to moderate pool size. Currently it's static with pool_start_size.
   TaskQueue(unsigned queue_size, unsigned pool_start_size, unsigned pool_max_size);
 
-  template <typename F> bool TryAdd(F&& f) {
-    return queue_.TryAdd(std::forward<F>(f));
-  }
-
-  // Returns true if task queue was blocked when adding the task.
+  // Returns true if Add() had to preempt (block) because the queue was full.
   template <typename F> bool Add(F&& f) {
-    if (queue_.TryAdd(std::forward<F>(f)))
-      return false;
-
-    ++blocked_submitters_;
-    auto res = queue_.Add(std::forward<F>(f));
-    --blocked_submitters_;
-    return res;
+    return queue_.Add(std::forward<F>(f));
   }
 
   template <typename F> auto Await(F&& f) -> decltype(f()) {
-    util::fb2::Done done;
-    using ResultType = decltype(f());
-    util::detail::ResultMover<ResultType> mover;
-
-    ++blocked_submitters_;
-    Add([&mover, f = std::forward<F>(f), done]() mutable {
-      mover.Apply(f);
-      done.Notify();
-    });
-    --blocked_submitters_;
-    done.Wait();
-    return std::move(mover).get();
+    return queue_.Await(std::forward<F>(f));
   }
 
   /**
@@ -60,15 +39,15 @@ class TaskQueue {
    */
   void Shutdown();
 
+  // Number of submitters on the current thread currently blocked adding into a full task queue.
+  // Contention gauge maintained by FiberQueue::Add.
   static unsigned blocked_submitters() {
-    return blocked_submitters_;
+    return util::fb2::FiberQueue::blocked_submitters();
   }
 
  private:
   util::fb2::FiberQueue queue_;
   std::vector<util::fb2::Fiber> consumer_fibers_;
-
-  static __thread unsigned blocked_submitters_;
 };
 
 }  // namespace dfly
