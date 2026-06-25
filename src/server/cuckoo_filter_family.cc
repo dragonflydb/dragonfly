37cab23fbd4d8267bc26bba4ad93ab10bcd279db
Kostas Kyrimis <kostas@dragonflydb.io>
2026-06-25 11:54:26 +0300

    chore: add cf.exists and cf.mexists (#7682)
    Signed-off-by: Kostas Kyrimis <kostas@dragonflydb.io>

--- src/server/cuckoo_filter_family.cc
+++ src/server/cuckoo_filter_family.cc
@@ -55,6 +55,20 @@ OpResult<bool> OpAddNx(const OpArgs& op_args, string_view key, string_view item)
   return true;
 }
 
+OpResult<vector<bool>> OpExists(const OpArgs& op_args, string_view key, ParsedArgs items) {
+  auto& db_slice = op_args.GetDbSlice();
+  auto op_res = db_slice.FindReadOnly(op_args.db_cntx, key, OBJ_CUCKOOFILTER);
+  if (!op_res)
+    return op_res.status();
+
+  const CuckooFilter* cf = op_res.value()->second.GetCuckooFilter();
+  vector<bool> result(items.size());
+  for (size_t i = 0; i < items.size(); ++i) {
+    result[i] = cf->Exists(CuckooFilter::Hash(items[i]));
+  }
+  return result;
+}
+
 OpStatus OpReserve(const OpArgs& op_args, string_view key, const CuckooFilterOptions& options) {
   auto& db_slice = op_args.GetDbSlice();
   auto op_res = db_slice.AddOrFind(op_args.db_cntx, key, OBJ_CUCKOOFILTER);
@@ -67,8 +81,7 @@ OpStatus OpReserve(const OpArgs& op_args, string_view key, const CuckooFilterOpt
   return OpStatus::OK;
 }
 
-void CmdReserve(CmdArgList args, CommandContext* cmd_cntx) {
-  CmdArgParser parser(cmd_cntx->tail_args());
+void CmdReserve(CmdArgParser parser, CommandContext* cmd_cntx) {
   string_view key = parser.Next();
   uint64_t capacity = parser.Next<uint64_t>();
   auto* rb = static_cast<RedisReplyBuilder*>(cmd_cntx->rb());
@@ -118,9 +131,9 @@ void CmdReserve(CmdArgList args, CommandContext* cmd_cntx) {
   return rb->SendError(res);
 }
 
-void CmdAdd(CmdArgList args, CommandContext* cmd_cntx) {
-  string_view key = ArgS(args, 0);
-  string_view item = ArgS(args, 1);
+void CmdAdd(CmdArgParser parser, CommandContext* cmd_cntx) {
+  string_view key = parser.Next();
+  string_view item = parser.Next();
 
   const auto cb = [&](Transaction* t, EngineShard* shard) {
     return OpAdd(t->GetOpArgs(shard), key, item);
@@ -132,9 +145,9 @@ void CmdAdd(CmdArgList args, CommandContext* cmd_cntx) {
   cmd_cntx->SendLong(*res);
 }
 
-void CmdAddNx(CmdArgList args, CommandContext* cmd_cntx) {
-  string_view key = ArgS(args, 0);
-  string_view item = ArgS(args, 1);
+void CmdAddNx(CmdArgParser parser, CommandContext* cmd_cntx) {
+  string_view key = parser.Next();
+  string_view item = parser.Next();
 
   const auto cb = [&](Transaction* t, EngineShard* shard) {
     return OpAddNx(t->GetOpArgs(shard), key, item);
@@ -144,6 +157,38 @@ void CmdAddNx(CmdArgList args, CommandContext* cmd_cntx) {
   if (!res)
     return cmd_cntx->SendError(res.status());
   cmd_cntx->SendLong(*res);
+}
+
+OpResult<vector<bool>> RunExists(CommandContext* cmd_cntx, string_view key, ParsedArgs items) {
+  const auto cb = [&](Transaction* t, EngineShard* shard) {
+    return OpExists(t->GetOpArgs(shard), key, items);
+  };
+  return cmd_cntx->tx()->ScheduleSingleHopT(std::move(cb));
+}
+
+void CmdExists(CmdArgParser parser, CommandContext* cmd_cntx) {
+  string_view key = parser.Next();
+  ParsedArgs items = parser.UnparsedArgs();
+
+  OpResult<vector<bool>> res = RunExists(cmd_cntx, key, items);
+  if (!res && res.status() != OpStatus::KEY_NOTFOUND && res.status() != OpStatus::WRONG_TYPE)
+    return cmd_cntx->SendError(res.status());
+  cmd_cntx->SendLong(res ? res->front() : 0);
+}
+
+void CmdMExists(CmdArgParser parser, CommandContext* cmd_cntx) {
+  string_view key = parser.Next();
+  ParsedArgs items = parser.UnparsedArgs();
+
+  OpResult<vector<bool>> res = RunExists(cmd_cntx, key, items);
+  if (!res && res.status() != OpStatus::KEY_NOTFOUND && res.status() != OpStatus::WRONG_TYPE)
+    return cmd_cntx->SendError(res.status());
+
+  auto* rb = static_cast<RedisReplyBuilder*>(cmd_cntx->rb());
+  RedisReplyBuilder::ArrayScope scope{rb, items.size()};
+  for (size_t i = 0; i < items.size(); ++i) {
+    rb->SendLong(res ? static_cast<long>((*res)[i]) : 0);
+  }
 }
 
 }  // namespace
@@ -157,7 +202,9 @@ void RegisterCuckooFilterFamily(CommandRegistry* registry) {
 
   *registry << CI{"CF.RESERVE", CO::JOURNALED | CO::DENYOOM | CO::FAST, -3, 1, 1}.HFUNC(Reserve)
             << CI{"CF.ADD", CO::JOURNALED | CO::DENYOOM | CO::FAST, 3, 1, 1}.HFUNC(Add)
-            << CI{"CF.ADDNX", CO::JOURNALED | CO::DENYOOM | CO::FAST, 3, 1, 1}.HFUNC(AddNx);
+            << CI{"CF.ADDNX", CO::JOURNALED | CO::DENYOOM | CO::FAST, 3, 1, 1}.HFUNC(AddNx)
+            << CI{"CF.EXISTS", CO::READONLY | CO::FAST, 3, 1, 1}.HFUNC(Exists)
+            << CI{"CF.MEXISTS", CO::READONLY | CO::FAST, -3, 1, 1}.HFUNC(MExists);
 }
 
 }  // namespace dfly
