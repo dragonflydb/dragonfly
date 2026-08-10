77e40836f94f6d019e27a4a8a249261607cc4306
Kostas Kyrimis <kostas@dragonflydb.io>
2026-08-10 18:18:57 +0300

    chore: increase load for test_replicaof_reject_on_load
    Signed-off-by: Kostas Kyrimis <kostas@dragonflydb.io>

--- tests/dragonfly/redis_replication_test.py
+++ tests/dragonfly/redis_replication_test.py
@@ -218,6 +218,7 @@ replication_specs = [
 ]
 
 
+@pytest.mark.large
 @pytest.mark.parametrize("t_replicas, seeder_config", replication_specs)
 async def test_redis_replication_all(
     df_factory: DflyInstanceFactory,
