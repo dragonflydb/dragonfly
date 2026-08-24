4339cd45686a625c816a4247354cccb39b93de35
Kostas Kyrimis <kostas@dragonflydb.io>
2026-08-24 11:42:43 +0300

    feat(server): throttle defrag task CPU duty cycle (#8115)
    Signed-off-by: Kostas Kyrimis <kostas@dragonflydb.io>

--- src/server/server_family.cc
+++ src/server/server_family.cc
@@ -2844,6 +2844,7 @@ string ServerFamily::FormatInfoMetrics(
     append("defrag_attempt_total", m.shard_stats.defrag_attempt_total);
     append("defrag_realloc_total", m.shard_stats.defrag_realloc_total);
     append("defrag_task_invocation_total", m.shard_stats.defrag_task_invocation_total);
+    append("async_delete_task_invocation_total", m.shard_stats.async_delete_task_invocation_total);
     append("borrowed_strings_sent_total", reply_stats.borrowed_string_sent_cnt);
 
     // Number of connections that are currently blocked on grabbing interpreter.
