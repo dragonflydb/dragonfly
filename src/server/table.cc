c05378f745fba1cd549bd25f304162f3ba6301b7
Kostas Kyrimis <kostas@dragonflydb.io>
2026-09-18 16:58:00 +0300

    leak
    Signed-off-by: Kostas Kyrimis <kostas@dragonflydb.io>

--- src/server/table.cc
+++ src/server/table.cc
@@ -121,6 +121,7 @@ DbTable::~DbTable() {
   DCHECK_EQ(thread_index, ServerState::tlocal()->thread_index());
   delete sample_top_keys;
   delete sample_unique_keys;
+  delete sample_values_hist;
 }
 
 void DbTable::PrepareForSingleShotHeapDestroy() {
