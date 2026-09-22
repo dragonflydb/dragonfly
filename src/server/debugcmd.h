da185746d0ef2799d7c5ea9c3d4497d0d221b522
Kostas Kyrimis <kostas@dragonflydb.io>
2026-09-22 14:34:41 +0300

    clean
    Signed-off-by: Kostas Kyrimis <kostas@dragonflydb.io>

--- src/server/debugcmd.h
+++ src/server/debugcmd.h
@@ -57,8 +57,6 @@ class DebugCmd {
   // Investigation-only. Remove once closed.
   void ReplDiag(CommandContext* cmd_cntx);
   void Shards(CommandContext* cmd_cntx);
-  // Test-only: OOB vector write -> UB crash, to test core dump capture.
-  void VectorOob(CommandContext* cmd_cntx);
   void LogTraffic(facade::CmdArgParser parser, CommandContext* cmd_cntx);
   void RecvSize(std::string_view param, CommandContext* cmd_cntx);
   void Topk(facade::CmdArgParser parser, CommandContext* cmd_cntx);
